6397016f0854a9135079f01320e8cffe70e06cc9
Bjoern Winkler <git@web.bjoern-winkler.de>
2019-12-01 22:02:49 +0100

    2017 3

--- 2017_3/aoc2017_3.py
+++ 2017_3/aoc2017_3.py
@@ -1,9 +1,10 @@
 import math
 
+# part 1 - solution: 371
 input = 368078
 
 
-n = 21
+n = input
 
 def next_square(n):
     root = math.ceil(math.sqrt(n))
@@ -11,14 +12,28 @@ def next_square(n):
         root += 1
     return root
 
-length = next_square(n)
+# how long is the side of the square? Round up square root of the target number to next odd number
+side_length = next_square(n)
+# how far is the middle of the side from the corner?
+side_mid = (side_length - 1) // 2
 
-# mid = length ** 2 - ((length - 1) // 2)
-mid = length ** 2
+# number in the middle of the side with the highest number in the bottom right corner (e.g. 25 - mid = 23)
+mid = side_length ** 2 - side_mid
+# how far is the target number from the mid point of the side (modula side_length so it doesn't matter on which side)
+dist = abs(mid - n) % (side_length - 1)
 
-dist = abs(mid - n) % length
+corner_offset = abs(dist - side_mid)
+
+# max distance - from the corner
+max_dist = side_length - 1
+
+target_dist = max_dist - corner_offset
 
 print('n: ', n)
-print('length: ', length)
+print('length: ', side_length)
 print('mid: ', mid)
 print('dist: ', dist)
+print('corner_offset: ', corner_offset)
+print('max_dist: ', max_dist)
+print('target_dist: ', target_dist)
+
