e2fba0659edcbad2bb7a17b58bcd7308a442027c
Bjoern Winkler <bjrnwnklr@gmail.com>
2020-02-02 22:52:51 +0100

    2019 day 19 pt 2 wip

--- 19/aoc2019_19.py
+++ 19/aoc2019_19.py
@@ -69,7 +69,6 @@ if __name__ == '__main__':
     ### part 2
 
     grid_array = np.array(grid)
-    # empty array with square sums
     grid_square_sum = np.zeros(grid_array.shape)
 
     # Get the vector from 0,0 to left and right boundary in line 100
@@ -78,16 +77,48 @@ if __name__ == '__main__':
     right = np.max(np.argwhere(grid_array[y] == 1))
     logging.info(f'Row {y}: Left: {left}, Right: {right}')
 
-    # in radians
+    # calculate angle from -y axis to beam in radians
     phi_left = np.arctan2(left, y)
     phi_right = np.arctan2(right, y)
     logging.info(f'Angles to y. Left: {phi_left}, right: {phi_right}')
 
-    
+    # solution using numpy
+
+    # side length of square
+    square = 100
+
+    # y length to try
+    y_max = 10000
+
+    # calculate np.arrays with the coordinates for each y value
+    # start from height of square
+    bottom_left_y = np.arange(square - 1, y_max)
+    bottom_left_x = np.around(np.tan(phi_left) * bottom_left_y)
+    bottom_right_x = np.around(np.tan(phi_left) * bottom_left_y)
+
+
+
+    # calculate the top right values, this time starting from 0
+    top_right_y = np.arange(y_max - square + 1)
+    top_right_x = np.around(np.tan(phi_right) * top_right_y)
+    # top_right_x = np.around(np.tan(phi_right) * bottom_left_y)
+
+    # now find all values where x_right - x_left >= square
+    dx = (top_right_x - bottom_left_x) >= (square - 1)
+    min_top_right_x = np.min(np.argwhere(dx == True))
+    min_top_left_y = np.argwhere(top_right_x == min_top_right_x)
+
+    part_2_coords = (int(min_top_left_y), int(bottom_left_x[min_top_left_y]))
+
+    logging.debug(f'dx: {np.sum(dx)}')
+    logging.debug(f'Smallest top right x: {min_top_right_x} at y pos: {min_top_left_y}')
+    logging.info(f'top left corner of square: {part_2_coords}, answer = {part_2_coords[0] + part_2_coords[1] * 10000}')
+
+    # Wrong answer: INFO:root:top left corner of square: (1198, 947), answer = 9471198 (too high!)
 
     """
-    # side length of square
-    square = 4
+    for y in range(square - 1, 45):
+        logging.debug(f'y: {y}, bottom_left_x: {bottom_left_x[y]}, top_right_x: {top_right_x[y]}')
 
     for y in range(n_y - square):
         for x in range(n_x - square):
@@ -108,7 +139,7 @@ if __name__ == '__main__':
 
     logging.info(f'Closest coordinates: {result_2}, answer = {result_2[0] + result_2[1] * 10000}')
 
-
+    
     # print data for the 4 lines with smallest square
     for y in range(result_2[0], result_2[0] + square):
         line_left = np.min(np.argwhere(grid_array[y] == 1))
