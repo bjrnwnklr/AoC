37cfd56fa3b2514ad6467ba395da4451d6163ac4
Bjoern Winkler <bjrnwnklr@gmail.com>
2020-03-29 22:04:56 +0200

    ackermann function

--- synacor/f_6027.py
+++ synacor/f_6027.py
@@ -1,32 +1,37 @@
-def f_6027():
-    global r0
-    global r1
-    print(f'Top, r0: {r0}, r1: {r1}')
-    if not r0:
-        # print(f'2nd-a, r0: {r0}, r1: {r1}')
-        r0 = r1 + 1
-        # print(f'2nd-b, r0: {r0}, r1: {r1}')
-        return # return what?
-    if not r1:
-        # print(f'3rd-a, r0: {r0}, r1: {r1}')
-        r0 -= 1
-        r1 = r7
-        f_6027()
-        # print(f'3rd-b, r0: {r0}, r1: {r1}')
-        return # return what?
-    r0_tmp = r0 # push r0
-    r1 -= 1
-    f_6027()
-    r1 = r0
-    r0 = r0_tmp
-    r0 -= 1
-    f_6027()
-    return
+from functools import lru_cache
+import sys
 
-r0 = 4
-r1 = 1
-r7 = 6
+@lru_cache(50000)
+def f_6027(m, n):
+    if m == 0:
+        return n + 1
+    elif n == 0:
+        return f_6027(m-1, k)
+    else:
+        return f_6027(m-1, f_6027(m, n-1))
 
+def f_6027_memo(m, n):
+    global memoization
+    if (m, n) in memoization:
+        return memoization[(m, n)]
+    else:
+        if m == 0:
+            memoization[(m, n)] = n + 1
+            return n + 1
+        elif n == 0:
+            r = f_6027(m-1, k)
+            memoization[(m, n)] = r
+            return r
+        else:
+            r = f_6027(m-1, f_6027(m, n-1))
+            memoization[(m, n)] = r
+            return r
+
+
+memoization = dict()
+sys.setrecursionlimit(1000000)
 print('starting')
-
-f_6027()
+k = 25468
+r = f_6027_memo(4, 0)
+print('ended')
+print(r)
