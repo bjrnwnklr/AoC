c716409884e51bb187a2ebe0cf66e42f6a9f851b
Bjoern Winkler <bjrnwnklr@gmail.com>
2020-05-10 22:43:00 +0200

    2017 day 20 done

--- 2017/20/aoc2017_20_2.py
+++ 2017/20/aoc2017_20_2.py
@@ -2,7 +2,7 @@ import re
 import numpy as np
 from collections import Counter
 
-f_name = 'ex2.txt'
+f_name = 'input.txt'
 
 with open(f_name, 'r') as f:
     reg_num = r'([-]*\d+)'
@@ -10,14 +10,13 @@ with open(f_name, 'r') as f:
 
 # np.array of the particles
 particles = np.array(particles_array)
-# status of particles (True = alive, False = destroyed)
-alive = np.full(particles.shape[0], True)
-# list of closest particle per cycle
-closest_per_cycle = []
+
+# list contains number of particles per cycle
+particles_per_cycle = []
 
 cycles = 0
-while cycles < 100:
-
+while cycles < 10000:
+    alive = np.full(particles.shape[0], True)
     # calculate new position
     for i in range(3):
         # calculate new velocity
@@ -26,11 +25,25 @@ while cycles < 100:
         particles[:, i] += particles[:, i+3]
 
     # calculate collisions
-    for i, p in enumerate(particles):
-        alive
+    # - create normal array
+    # - create tuples of position coordinates
+    # - count occurences, using a Counter
+    # - for every count > 1, remove all elements
+    # - create new particles np.array
+    part_list = particles.tolist()
+    pos_tuples = list(map(tuple, particles[:, :3].tolist()))
+    collisions = [item for item, count in Counter(pos_tuples).items() if count > 1]
+    # find position of all matches
+    for c in collisions:
+        for i, p in enumerate(part_list):
+            if tuple(p[:3]) == c:
+                alive[i] = False
 
-
+    particles = particles[alive]
+    particles_per_cycle.append(particles.shape[0])
     cycles += 1
 
-print(f'Particles left: {particles.shape[0]}')
+print(particles_per_cycle[-1])
 
+
+# part 2: 574
