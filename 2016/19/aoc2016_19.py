ae7c8a9c8361cedf9001d61daef3401149e038b9
Bjoern Winkler <bjrnwnklr@gmail.com>
2020-11-22 18:07:07 +0100

    2016 day 19 part 2

--- 2016/19/aoc2016_19.py
+++ 2016/19/aoc2016_19.py
@@ -8,7 +8,6 @@ elves = 3012210
 circle = deque(range(1, elves + 1))
 presents = {k: 1 for k in range(1, elves + 1)}
 
-
 # repeat until the circle only has one element
 while len(circle) > 1:
     # add presents from next elf to current elf
@@ -25,4 +24,26 @@ print(circle[0], presents[circle[0]])
 
 # Part 1: 1830117
 
+# ----- Part 2 -------
 
+# create two half circles of elves
+mid = elves // 2
+h1 = deque(range(1, mid + 1))
+h2 = deque(range(mid + 1, elves + 1))
+
+# repeat until the circle only has one element
+while len(h1) + len(h2) > 1:
+    # remove the first element of the 2nd half - the elf opposite of the current position
+    h2.popleft()
+    # move the current elf to the end of the 2nd half queue
+    h2.append(h1.popleft())
+    # if the overall length is odd, we also need to move the start of h2 to the end of h1 to balance
+    if (len(h1) + len(h2)) % 2 == 0:
+        h1.append(h2.popleft())
+
+if h1:
+    print(f'h1: {h1[0]}')
+if h2:
+    print(f'h2: {h2[0]}')
+
+# Part 2: 1417887
