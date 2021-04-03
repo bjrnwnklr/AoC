4fb04d51791211528a1565a0c6f704cfca607917
Bjoern Winkler <bjoern@bjoern-winkler.de>
2021-04-03 15:36:25 +0200

    2016 day 11: possible moves done

--- 2016/11/aoc2016_11.py
+++ 2016/11/aoc2016_11.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 import logging
+from itertools import combinations
 
 input_ex1 = [
     ('M', 'hydrogen', 1),
@@ -27,6 +28,7 @@ class Component:
         self.comp_element = comp_element
         self.floor = floor
         self.abbr = f'{self.comp_element[0].upper()}{self.comp_type}'
+        self.counterpart = f'{self.abbr[0]}{"G" if self.abbr[1] == "M" else "M"}'
 
     def __repr__(self):
         return f'{self.comp_element}-{self.comp_type}: f{self.floor}'
@@ -53,6 +55,37 @@ class FloorConfig:
     def move_component(self, abbr, floor):
         self.components[abbr].floor = floor
 
+    def is_valid(self):
+        """
+        Check if the current state represents a valid state, or if any chips on any floor get fried.
+
+        Chips get fried if there is a generator without its associated microchip and another microchip without generator
+        on the same floor.
+        :return: True if the current state is valid, False otherwise
+        """
+        fried = False
+        floor = 1
+        while not fried and floor <= 4:
+            # get all components on the floor that are single type (i.e. their counterpart is on a different floor)
+            single_comps_on_floor = {c for c in self.components if
+                                        self.components[c].floor == floor and
+                                        self.components[self.components[c].counterpart].floor != floor}
+            logging.debug(f'Single elements on floor {floor}: {single_comps_on_floor}')
+
+            # if the more than one component is on the floor, check if there are any
+            # (M)icrochips and (G)enerators together
+            if len(single_comps_on_floor) > 1:
+                types_on_floor = {c[1] for c in single_comps_on_floor}
+                # if there is more than on component type on the floor (i.e. microchip and generator together),
+                # chips will get fried and the state is invalid
+                if len(types_on_floor) > 1:
+                    logging.debug(f'FRIED!!! Floor {floor}: {single_comps_on_floor}')
+                    fried = True
+
+            floor += 1
+        logging.debug(f'State is valid: {not fried}')
+        return not fried
+
     def __repr__(self):
         representation = ''
         for floor in range(4, 0, -1):
@@ -78,6 +111,30 @@ class FloorConfig:
         next_floors = [self.elevator + i for i in [-1, 1] if 1 <= self.elevator + i <= 4]
         logging.debug(f'Next floors: {next_floors}')
 
+        # get combinations of components on the current floor
+        comps_on_floor = [c for c in self.components if self.components[c].floor == self.elevator]
+        logging.debug(f'Components on current floor: {comps_on_floor}')
+        available_combinations = list(combinations(comps_on_floor, 1)) + list(combinations(comps_on_floor, 2))
+        logging.debug(f'1-2 combinations of available components: {available_combinations}')
+
+        # create a copy of the current state, move all combinations of elements to available floors
+        # and evaluate if it is a valid state. If yes, add it to a list
+        valid_states = []
+        for floor in next_floors:
+            for comb in available_combinations:
+                next_state = deepcopy(self)
+                # move the elevator
+                next_state.elevator = floor
+                # move the components
+                for c in comb:
+                    next_state.move_component(c, floor)
+                logging.debug(f'Created copy for move to floor {floor} with components {comb}:\n{next_state}')
+                if next_state.is_valid():
+                    valid_states.append(next_state)
+
+        logging.debug(f'Found {len(valid_states)} valid states.')
+        return valid_states
+
 
 
 if __name__ == '__main__':
@@ -92,17 +149,18 @@ if __name__ == '__main__':
     for c, e, f in inp:
         fc.add_component(Component(c, e, f))
 
-    print(fc)
-    print(fc.endstate)
+    logging.debug(f'Starting, initial configuration:\n{fc}')
+    logging.debug(f'Endstate: {fc.endstate}')
+    #
+    # # test deepcopy and changing an element in the copy
+    # fc_copy = deepcopy(fc)
+    #
+    # print(fc_copy)
+    #
+    # # fc_copy.components['HM'].floor = 2
+    # fc_copy.move_component('HM', 2)
+    # print(fc)
+    # print(fc_copy)
 
-    # test deepcopy and changing an element in the copy
-    fc_copy = deepcopy(fc)
-
-    print(fc_copy)
-
-    # fc_copy.components['HM'].floor = 2
-    fc_copy.move_component('HM', 2)
-    print(fc)
-    print(fc_copy)
-
-    fc.possible_moves()
+    next_moves = fc.possible_moves()
+    
