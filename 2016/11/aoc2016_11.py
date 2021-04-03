5ac51d20a4abdc9093382cf4b0f63d3da743184d
Bjoern Winkler <bjrnwnklr@gmail.com>
2021-04-03 12:28:59 +0200

    2016 day 11 possible_moves

--- 2016/11/aoc2016_11.py
+++ 2016/11/aoc2016_11.py
@@ -1,3 +1,26 @@
+from copy import deepcopy
+import logging
+
+input_ex1 = [
+    ('M', 'hydrogen', 1),
+    ('M', 'lithium', 1),
+    ('G', 'hydrogen', 2),
+    ('G', 'lithium', 3)
+]
+
+input_puz = [
+    ('G', 'strontium', 1),
+    ('M', 'strontium', 1),
+    ('G', 'plutonium', 1),
+    ('M', 'plutonium', 1),
+    ('G', 'thulium', 2),
+    ('G', 'ruthenium', 2),
+    ('M', 'ruthenium', 2),
+    ('G', 'curium', 2),
+    ('M', 'curium', 2),
+    ('M', 'thulium', 3)
+]
+
 class Component:
     def __init__(self, comp_type, comp_element, floor):
         self.comp_type = comp_type
@@ -12,41 +35,74 @@ class Component:
 class FloorConfig:
     def __init__(self):
         self.elevator = 1
-        self.floors = {
-            i: set()
-            for i in range(1, 5)
-        }
+        # dictionary with component abbreviation (e.g. 'HG' for hydrogen generator): component object pairs
         self.components = dict()
         self.endstate = '4'
 
     def add_component(self, component):
-        self.floors[component.floor].add(component)
-        self.components[component.abbr] = component.floor
+        self.components[component.abbr] = component
         # update endstate
         self.endstate += '4'
 
     def current_state(self):
         cs = f'{str(self.elevator)}'
-        for c, f in sorted(self.components.items()):
-            cs += f'{str(f)}'
+        for c in sorted(self.components):
+            cs += f'{str(self.components[c].floor)}'
         return cs
+
+    def move_component(self, abbr, floor):
+        self.components[abbr].floor = floor
 
     def __repr__(self):
         representation = ''
         for floor in range(4, 0, -1):
             representation += f'F{floor}\t'
             representation += 'E\t' if self.elevator == floor else '\t'
-            representation += '\t'.join(c.abbr for c in self.floors[floor]) + '\n'
+            representation += '\t'.join(c.abbr for c in self.components.values() if c.floor == floor) + '\n'
         representation += self.current_state() + '\n'
         return representation
 
+    def possible_moves(self):
+        """
+        Generate a list of possible moves, i.e. possible states. This will generate all possible combinations
+        of moving 1-2 components from the current floor to the floors -1 and +1 and check if the new state generated
+        is possible, i.e. that no chips get fried.
+
+        The method will return a list of copies of the current state with the respective components and the
+        elevator moved to the new floor.
+
+        This list can then be used to run a BFS on the possible next moves.
+        :return: A list of FloorConfig objects, representing a possible next move.
+        """
+        # get list of possible floors
+        next_floors = [self.elevator + i for i in [-1, 1] if 1 <= self.elevator + i <= 4]
+        logging.debug(f'Next floors: {next_floors}')
+
+
 
 if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.DEBUG)
+
     fc = FloorConfig()
-    fc.add_component(Component('M', 'hydrogen', 1))
-    fc.add_component(Component('M', 'lithium', 1))
-    fc.add_component(Component('G', 'hydrogen', 2))
-    fc.add_component(Component('G', 'lithium', 3))
+
+    inp = input_ex1
+    # inp = input_puz
+
+    for c, e, f in inp:
+        fc.add_component(Component(c, e, f))
 
     print(fc)
     print(fc.endstate)
+
+    # test deepcopy and changing an element in the copy
+    fc_copy = deepcopy(fc)
+
+    print(fc_copy)
+
+    # fc_copy.components['HM'].floor = 2
+    fc_copy.move_component('HM', 2)
+    print(fc)
+    print(fc_copy)
+
+    fc.possible_moves()
