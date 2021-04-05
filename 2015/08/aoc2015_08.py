a6b2ea6eb07a775a306cfea904729350cf2e0d6d
Bjoern Winkler <bjoern@bjoern-winkler.de>
2021-04-05 13:56:26 +0200

    2015 day 08 part 2 done

--- 2015/08/aoc2015_08.py
+++ 2015/08/aoc2015_08.py
@@ -1,7 +1,7 @@
 import re
 
-REGEX = re.compile(r'(\"|\\x..|\\[\\\"])', re.UNICODE)
-
+REGEX_LITERAL = re.compile(r'(\"|\\x..|\\[\\\"])', re.UNICODE)
+REGEX_ENCODED = re.compile(r'(\"|\\)', re.UNICODE)
 
 def count_code(line):
     return len(line)
@@ -16,8 +16,14 @@ def count_literal(line):
             return 'X'
         else:
             return 'X'
-    newline = REGEX.sub(_sub_escapes, line)
+    newline = REGEX_LITERAL.sub(_sub_escapes, line)
     return len(newline)
+
+def count_encoded(line):
+    def _sub_encoded(matchobj):
+        return 'XX'
+    newline = REGEX_ENCODED.sub(_sub_encoded, line)
+    return len(newline) + 2  # add 2 for surrounding quotes
 
 
 if __name__ == '__main__':
@@ -28,14 +34,15 @@ if __name__ == '__main__':
     with open(f_name, 'r', encoding="utf-8") as f:
         lines = f.readlines()
 
-    c_code = c_literal = 0
+    c_code = c_literal = c_encoded = 0
     for line in lines:
         line = line.strip()
         c_code += count_code(line)
         c_literal += count_literal(line)
+        c_encoded += count_encoded(line)
 
     print(f'code: {c_code} - literal: {c_literal} = {c_code - c_literal}')
+    print(f'encoded: {c_encoded} - code: {c_code} = {c_encoded - c_code}')
 
     # Part 1. 1342
-
-    
+    # Part 2: 2074
