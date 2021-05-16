e1d43ee33ecd853612c2d42e520b2c2f9f6aef1e
Bjoern Winkler <bjrnwnklr@gmail.com>
2021-05-16 20:19:13 +0200

    2015 day 19 part 2 in progress

--- 2015/19/aoc2015_19.py
+++ 2015/19/aoc2015_19.py
@@ -1,5 +1,5 @@
 import re
-from collections import defaultdict
+from collections import defaultdict, deque
 
 
 def sub_iter(pattern, repl, text):
@@ -8,8 +8,8 @@ def sub_iter(pattern, repl, text):
 
 if __name__ == '__main__':
 
-    f_name = 'ex1.txt'
-    # f_name = 'input.txt'
+    # f_name = 'ex1.txt'
+    f_name = 'input.txt'
 
     with open(f_name, 'r') as f:
         top, bottom = f.read().split('\n\n')
@@ -33,5 +33,27 @@ if __name__ == '__main__':
     # Part 2 starts here
     start = 'e'
     end = cal_molecule
+    seen = set()
+    q = deque([('e', 0)])
 
+    # do a BFS to find the end molecule
+    while q:
+        curr_mol, curr_steps = q.pop()
 
+        if curr_mol in seen:
+            continue
+
+        seen.add(curr_mol)
+
+        if curr_mol == end:
+            # we found the end result
+            print(f'Molecule reached after {curr_steps} steps.')
+            break
+
+        # get all valid neighbors - all possible substitutions
+        possible_subs = {k for k in recipes.keys() if k in curr_mol}
+        for pattern in possible_subs:
+            for repl in recipes[pattern]:
+                for new_text in sub_iter(pattern, repl, curr_mol):
+                    if len(new_text) <= len(end):
+                        q.appendleft((new_text, curr_steps + 1))
