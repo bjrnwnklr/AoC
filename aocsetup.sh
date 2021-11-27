cc2a968b7a0b306a52ca82a5e3ab4df1c122a44e
Bjoern Winkler <bjoern@bjoern-winkler.de>
2021-11-27 18:47:55 +0100

    initial version of aocsetup.sh

---
+++ aocsetup.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+############################################################
+# AoC Setup script.
+#
+# - Creates a folder for the AoC day in the folder it is 
+#   called from
+# - Copies the contents of the `template` folder into the
+#   new folder
+# - Renames the files to the `aoc<year>_<day>.py` format
+#   and changes references in the python files to the
+#   correct year and day.
+#
+# v1.0, 27 Nov 2021
+# Bjoern Winkler
+############################################################
+
+# Set variables
+Version="1.0"
+Date="27 Nov 2021"
+
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "Sets up a day folder for Advent of Code. Creates a new <day> folder in"
+   echo "the current folder, if not existing, then copies files from the"
+   echo "'template' folder into the new folder and does some renaming."
+   echo
+   echo "Syntax: aocsetup [-h|v|V] -y YEAR -d DAY"
+   echo "Set up Advent of Code folder <DAY> in current directory."
+   echo
+   echo "Options:"
+   echo "   -h     Print this Help."
+   echo "   -v     Verbose mode."
+   echo "   -V     Print software version and exit."
+   echo
+}
+
+############################################################
+# Version                                                  #
+############################################################
+Version()
+{
+   # Display Help
+   echo "aocsetup ${Version}, ${Date}"
+   echo
+}
+
+############################################################
+############################################################
+# Main program                                             #
+############################################################
+############################################################
+
+############################################################
+# Process the input options. Add options as needed.        #
+############################################################
+# Get the options
+while getopts ":hVy:d:" option; do
+    case $option in
+        h) # display Help
+            Help
+            exit;;
+        V) # display Version and exit
+            Version
+            exit;;
+        y) # the AoC Year
+            YEAR=$OPTARG;;
+        d) # the AoC day
+            DAY=$OPTARG;;
+        \?) # Invalid option
+            echo "Error: Invalid option"
+            exit;;
+    esac
+done
+
+# check if date and year have been Set
+if [[ -z ${YEAR} ]];
+then
+    echo "Year not specified, exiting."
+    exit;
+fi
+
+if [[ -z ${DAY} ]];
+then
+    echo "Day not specified, exiting."
+    exit;
+fi
+
+############################################################
+# Check if Day folder exists, if not create it.
+############################################################
+if [[ -d ${DAY} ]];
+then
+    echo "Folder ${DAY} already exists, exiting."
+    exit;
+else
+    mkdir ${DAY}
+fi
+
+############################################################
+# Copy templates folder contents into the new Day folder
+############################################################
+# get the directory the script is run from
+# https://stackoverflow.com/a/246128
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# check if templates directory exists
+if [[ -d "${SCRIPT_DIR}/template" ]];
+then
+    cp -r "${SCRIPT_DIR}"/template/* "${DAY}"
+else
+    echo "Template directory not found in ${SCRIPT_DIR}, exiting."
+    exit;
+fi
+
+############################################################
+# Rename the files with YEAR and DAY
+############################################################
+
+echo "Called from ${SCRIPT_DIR}"
