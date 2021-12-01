080f2a0c15d861e313e26da65faab11f7e8041fe
Bjoern Winkler <bjoern@bjoern-winkler.de>
2021-12-01 19:39:47 +0100

    shortened docstrings in template

--- template/aocyyyy_dd.py
+++ template/aocyyyy_dd.py
@@ -4,23 +4,13 @@
 # from collections import defaultdict
 
 def load_input(f_name):
-    """Loads the puzzle input from the specified file. Specify the relative path 
-    if loading files from a subdirectory, e.g. for loading test inputs, specify
-    `test/test1_1.txt`.
+    """Loads the puzzle input from the specified file. 
 
-    Depending on the puzzle, change how the lines in the file are parsed, what format
-    the extracted values have etc.
-
-    Args:
-        f_name (String): File name of the input file.
-
-    Returns:
-        List: A list of the inputs read in.
+    Specify the relative path if loading files from a subdirectory, 
+    e.g. for loading test inputs, specify `test/test1_1.txt`.
     """
     with open(f_name, 'r') as f:
         puzzle_input = []
-        # CHANGE HERE: depending on the input file, change how the lines are processed and what
-        # happens with the extracted values, e.g. convert to Integer.
         for line in f.readlines():
             puzzle_input.append(line.strip())
 
@@ -28,31 +18,13 @@ def load_input(f_name):
 
 
 def part1(puzzle_input):
-    """Solve part 1. Return the required output value.
-
-    Args:
-        puzzle_input (List): Typically a list of the input values from the input.txt puzzle input.
-
-    Returns:
-        Depends...: Typically an Integer value, but often also a String - this can be used on adventofcode 
-        as the answer to the puzzle.
-    """
-    # Add code here
+    """Solve part 1. Return the required output value."""
 
     return 1
 
 
 def part2(puzzle_input):
-    """Solve part 2. Return the required output value.
-
-    Args:
-        puzzle_input (List): Typically a list of the input values from the input.txt puzzle input.
-
-    Returns:
-        Depends...: Typically an Integer value, but often also a String - this can be used on adventofcode 
-        as the answer to the puzzle.
-    """
-    # Add code here
+    """Solve part 2. Return the required output value."""
 
     return 1
 
