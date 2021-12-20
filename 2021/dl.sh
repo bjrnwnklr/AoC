f83d85cbae307118529b89cd814f48fa47077b5e
Bjoern Winkler <bjoern@bjoern-winkler.de>
2021-12-20 20:34:40 +0100

    added days 14-20

---
+++ 2021/dl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in {14..20}; do
+	../aocsetup.sh 2021 ${i}
+	curl https://adventofcode.com/2021/day/${i}/input --cookie "session=53616c7465645f5f58b5e33ca37677c1533e760352f12a2abffdb824483d7ec902266a88e062b8ee86fb2962354a04d0" > input/${i}.txt
+	sleep 1
+	curl https://adventofcode.com/2021/day/${i} --cookie "session=53616c7465645f5f58b5e33ca37677c1533e760352f12a2abffdb824483d7ec902266a88e062b8ee86fb2962354a04d0" > puzzles/${i}.html
+	sleep 2
+done
---
+++ 2021/tests/test_aoc2021_18.py
@@ -0,0 +1,22 @@
+"""Test the examples given in the puzzle to verify the solution is working."""
+
+# load the required functions from the actual solution
+from solutions.aoc2021_18 import load_input, part1, part2
+
+
+class Test_AOC2021_18:
+    """Specifies the tests for parts 1 and 2.
+
+    Create test_n_n functions for each example given in the puzzle definition, put the example in a 
+    `test_n_n.txt` file in the `test` directory, and replace the expected value in the `assert` statement.
+
+    Tests can then be run in the day's directory with `pytest`.
+    """
+
+    def test_1_1(self):
+        puzzle_input = load_input('testinput/18_1_1.txt')
+        assert part1(puzzle_input) == 1
+
+    # def test_2_1(self):
+    #     puzzle_input = load_input('testinput/18_1_1.txt')
+    #     assert part2(puzzle_input) == 1
