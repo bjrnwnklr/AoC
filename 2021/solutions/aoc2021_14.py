17c762975bfbd9ef2015039445a3b4a6719539ad
Bjoern Winkler <bjoern@bjoern-winkler.de>
2021-12-24 11:42:11 +0100

    2021 day 14 pt 1 done

--- 2021/solutions/aoc2021_14.py
+++ 2021/solutions/aoc2021_14.py
@@ -1,7 +1,7 @@
 # Load any required modules. Most commonly used:
 
 # import re
-# from collections import defaultdict
+from collections import Counter
 # from utils.aoctools import aoc_timer
 
 
@@ -12,18 +12,52 @@ def load_input(f_name):
     e.g. for loading test inputs, specify `testinput/01_1_1.txt`.
     """
     with open(f_name, 'r') as f:
+
+        top, bottom = f.read().strip().split('\n\n')
+
+        poly_template = top.strip()
+
         puzzle_input = []
-        for line in f.readlines():
-            puzzle_input.append(line.strip())
+        for line in bottom.split('\n'):
+            puzzle_input.append(line.strip().split(' -> '))
 
-    return puzzle_input
+    return (poly_template, puzzle_input)
 
 
+def process_polymer(poly, rules):
+    new_poly = ''
+    for pp in zip(poly[:], poly[1:]):
+        pair = ''.join(pp)
+        # only add the first element and then the new element, otherwise we
+        # would duplicate each middle element (as it is the 2nd of the first pair and
+        # the 1st of the second pair)
+        new_poly += pp[0] + rules[pair]
+
+    # add the last element as we don't add it in the iteration
+    new_poly += poly[-1]
+
+    return new_poly
 # @aoc_timer
-def part1(puzzle_input):
-    """Solve part 1. Return the required output value."""
 
-    return 1
+
+def part1(puzzle_input):
+    """Solve part 1. Return the required output value.
+
+    What do you get if you take the quantity of the most 
+    common element and subtract the quantity of the least common element?
+    """
+
+    poly_template, pair_rules = puzzle_input
+    rules = {x: y for x, y in pair_rules}
+
+    new_poly = poly_template
+    for _ in range(10):
+        new_poly = process_polymer(new_poly, rules)
+
+    # count the elements
+    c = Counter(new_poly)
+
+    return c.most_common()[0][1] - c.most_common()[-1][1]
 
 
 # @aoc_timer
@@ -45,5 +79,5 @@ if __name__ == '__main__':
     p2 = part2(puzzle_input)
     print(f'Part 2: {p2}')
 
-# Part 1: Start:  End:
-# Part 2: Start:  End:
+# Part 1: Start: 11:15 End: 11:41
+# Part 2: Start: 11:42 End:
--- 2021/tests/test_aoc2021_14.py
+++ 2021/tests/test_aoc2021_14.py
@@ -15,7 +15,7 @@ class Test_AOC2021_14:
 
     def test_1_1(self):
         puzzle_input = load_input('testinput/14_1_1.txt')
-        assert part1(puzzle_input) == 1
+        assert part1(puzzle_input) == 1588
 
     # def test_2_1(self):
     #     puzzle_input = load_input('testinput/14_1_1.txt')
