6d82fecf11af363b0e42e1c8e495a5e8979e2ae5
Bjoern Winkler <bjoern@bjoern-winkler.de>
2021-12-24 19:21:34 +0100

    2021 day 15 part 1 done

--- 2021/solutions/aoc2021_15.py
+++ 2021/solutions/aoc2021_15.py
@@ -3,6 +3,7 @@
 # import re
 # from collections import defaultdict
 # from utils.aoctools import aoc_timer
+from heapq import heappush, heappop
 
 
 def load_input(f_name):
@@ -14,16 +15,53 @@ def load_input(f_name):
     with open(f_name, 'r') as f:
         puzzle_input = []
         for line in f.readlines():
-            puzzle_input.append(line.strip())
+            puzzle_input.append(list(map(int, list(line.strip()))))
 
     return puzzle_input
+
+
+def neighbors(g, pos):
+    results = []
+    for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+        nr = pos[0] + dr
+        nc = pos[1] + dc
+        if (nr, nc) in g:
+            results.append((nr, nc))
+    return results
+
+
+def dijkstra(g, start, target):
+    """Run a Dijkstra search for the shortest path in the grid"""
+    q = [(0, start)]
+    seen = set()
+    while q:
+        (cost, cur_pos) = heappop(q)
+        if cur_pos not in seen:
+            seen.add(cur_pos)
+            if cur_pos == target:
+                return cost
+
+            for next_pos in neighbors(g, cur_pos):
+                if next_pos not in seen:
+                    heappush(q, (cost + g[next_pos], next_pos))
+
+
+def generate_grid(inp):
+    return {(r, c): val
+            for r, row in enumerate(inp)
+            for c, val in enumerate(row)
+            }
 
 
 # @aoc_timer
 def part1(puzzle_input):
     """Solve part 1. Return the required output value."""
 
-    return 1
+    g = generate_grid(puzzle_input)
+    target = (len(puzzle_input) - 1, len(puzzle_input[0]) - 1)
+    result = dijkstra(g, (0, 0), target)
+
+    return result
 
 
 # @aoc_timer
@@ -45,5 +83,5 @@ if __name__ == '__main__':
     p2 = part2(puzzle_input)
     print(f'Part 2: {p2}')
 
-# Part 1: Start:  End:
-# Part 2: Start:  End:
+# Part 1: Start: 19:01 End: 19:21
+# Part 2: Start: 19:22 End:
--- 2021/tests/test_aoc2021_15.py
+++ 2021/tests/test_aoc2021_15.py
@@ -15,7 +15,7 @@ class Test_AOC2021_15:
 
     def test_1_1(self):
         puzzle_input = load_input('testinput/15_1_1.txt')
-        assert part1(puzzle_input) == 1
+        assert part1(puzzle_input) == 40
 
     # def test_2_1(self):
     #     puzzle_input = load_input('testinput/15_1_1.txt')
