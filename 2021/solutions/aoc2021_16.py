14a52d3447d7ecf2bb1051c4b2816dd550287cb7
Bjoern Winkler <bjoern@bjoern-winkler.de>
2021-12-25 12:46:14 +0100

    2021 day 16 pt 1 wip

--- 2021/solutions/aoc2021_16.py
+++ 2021/solutions/aoc2021_16.py
@@ -6,23 +6,71 @@
 
 
 def load_input(f_name):
-    """Loads the puzzle input from the specified file. 
+    """Loads the puzzle input from the specified file.
 
-    Specify the relative path if loading files from a subdirectory, 
+    Specify the relative path if loading files from a subdirectory,
     e.g. for loading test inputs, specify `testinput/01_1_1.txt`.
     """
     with open(f_name, 'r') as f:
-        puzzle_input = []
-        for line in f.readlines():
-            puzzle_input.append(line.strip())
+        return f.readline().strip()
 
-    return puzzle_input
 
+def hex_to_bitlist(hex_inp: str) -> list[int]:
+    """
+    Convert a hex string e.g. D2FE28 into a list of bits (0 and 1 values).
+    Each hexadecimal character corresponds to four bits.
+    """
+    bitlist = []
+    for h in hex_inp:
+        h_int = int(h, base=16)
+        h_bin = f'{h_int:0>4b}'
+        bitlist.extend(list(h_bin))
+    return bitlist
+
+
+def bitlist_to_int(bitlist: list[int]) -> int:
+    """Convert a list of bits (0 and 1 values) into an integer. Most significant bit comes first."""
+    b = ''.join(x for x in bitlist)
+    return int(b, base=2)
+
+
+def packet_version(packet: list[int]) -> int:
+    """Parse the header of a packet and return the packet version (bits 0-2) as an int."""
+    return bitlist_to_int(packet[:3])
+
+
+def packet_type(packet: list[int]) -> int:
+    """Parse the header of a packet and return the packet type (bits 3-5) as an int."""
+    return bitlist_to_int(packet[3:6])
+
+
+def literal_value(packet: list[int]) -> int:
+    """Decode the value of a literal value type packet (packet_type == 4)."""
+    assert packet_type(packet) == 4
+    group_length = 5
+    payload = packet[6:]
+    num_groups = len(payload) // group_length
+    bitlist = []
+    for i in range(num_groups):
+        b = payload[i * group_length: (i * group_length) + group_length]
+        bitlist.extend(b[1:])
+
+    return bitlist_to_int(bitlist)
+
+
+def parse_packet(packet: list[int]):
+    """Parse a packet recursively and return the sum of the packet versions contained 
+    in all subpackets within."""
+    pass
 
 # @aoc_timer
+
+
 def part1(puzzle_input):
     """Solve part 1. Return the required output value."""
 
+    bitlist = hex_to_bitlist(puzzle_input)
+    literal_value(bitlist)
     return 1
 
 
@@ -45,5 +93,5 @@ if __name__ == '__main__':
     p2 = part2(puzzle_input)
     print(f'Part 2: {p2}')
 
-# Part 1: Start:  End:
+# Part 1: Start: 11:41 End:  (Reading the text and creating 7 test cases took until 11:59!)
 # Part 2: Start:  End:
--- 2021/tests/test_aoc2021_16.py
+++ 2021/tests/test_aoc2021_16.py
@@ -1,7 +1,7 @@
 """Test the examples given in the puzzle to verify the solution is working."""
 
 # load the required functions from the actual solution
-from solutions.aoc2021_16 import load_input, part1, part2
+from solutions.aoc2021_16 import load_input, part1, part2, hex_to_bitlist, literal_value
 
 
 class Test_AOC2021_16:
@@ -14,9 +14,73 @@ class Test_AOC2021_16:
     """
 
     def test_1_1(self):
-        puzzle_input = load_input('testinput/16_1_1.txt')
-        assert part1(puzzle_input) == 1
+        """Convert a hex input into a decimal number
 
-    # def test_2_1(self):
-    #     puzzle_input = load_input('testinput/16_1_1.txt')
-    #     assert part2(puzzle_input) == 1
+        This is a type 4 (literal) packet.
+
+        This input (D2FE28) is represented as bits: 110100101111111000101000
+        with the literal number encoded being 2021.
+        """
+        puzzle_input = load_input('testinput/16_1_1.txt')
+        bitlist = hex_to_bitlist(puzzle_input)
+        assert literal_value(bitlist) == 2021
+
+    # def test_1_2(self):
+    #     """Convert a hex input into a decimal number
+
+    #     This is a length type 0 packet containing two sub packets.
+
+    #     This input (38006F45291200) is represented as bits:
+    #     00111000000000000110111101000101001010010001001000000000
+    #     with the literal number encoded being 10 and 20 in the two sub packets.
+    #     """
+    #     puzzle_input = load_input('testinput/16_1_2.txt')
+    #     assert part1(puzzle_input) == 1020
+
+    # def test_1_3(self):
+    #     """Convert a hex input into a decimal number
+
+    #     This is a length type 1 packet containing two sub packets.
+
+    #     This input (EE00D40C823060) is represented as bits:
+    #     11101110000000001101010000001100100000100011000001100000
+    #     with the literal number encoded being 1, 2 and 3 in the three sub packets.
+    #     """
+    #     puzzle_input = load_input('testinput/16_1_3.txt')
+    #     assert part1(puzzle_input) == 123
+
+    # def test_1_4(self):
+    #     """
+    #     8A004A801A8002F478 represents an operator packet (version 4) which contains
+    #     an operator packet (version 1) which contains an operator packet (version 5)
+    #     which contains a literal value (version 6); this packet has a version sum of 16.
+    #     """
+    #     puzzle_input = load_input('testinput/16_1_4.txt')
+    #     assert part1(puzzle_input) == 16
+
+    # def test_1_5(self):
+    #     """
+    #     620080001611562C8802118E34 represents an operator packet (version 3) which contains
+    #     two sub-packets; each sub-packet is an operator packet that contains
+    #     two literal values. This packet has a version sum of 12.
+    #     """
+    #     puzzle_input = load_input('testinput/16_1_5.txt')
+    #     assert part1(puzzle_input) == 12
+
+    # def test_1_6(self):
+    #     """
+    #     C0015000016115A2E0802F182340 has the same structure as the previous example,
+    #     but the outermost packet uses a different length type ID.
+    #     This packet has a version sum of 23.
+    #     """
+    #     puzzle_input = load_input('testinput/16_1_6.txt')
+    #     assert part1(puzzle_input) == 23
+
+    # def test_1_7(self):
+    #     """
+    #     A0016C880162017C3686B18A3D4780 is an operator packet that contains an
+    #     operator packet that contains an operator packet that contains
+    #     five literal values; it has a version sum of 31.
+    #     """
+    #     puzzle_input = load_input('testinput/16_1_7.txt')
+    #     assert part1(puzzle_input) == 31
