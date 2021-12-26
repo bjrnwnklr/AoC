8378deb54fe2cd6e9c330bc9b58d42a180304e8f
Bjoern Winkler <bjoern@bjoern-winkler.de>
2021-12-26 18:49:13 +0100

    2021 day 18 convert and add implemented

--- 2021/solutions/aoc2021_18.py
+++ 2021/solutions/aoc2021_18.py
@@ -19,7 +19,39 @@ def load_input(f_name):
     return puzzle_input
 
 
+def add(a, b):
+    """Add two snailfish numbers a and b to each other and return the added number."""
+    return [a, b]
+
+
+def convert(sn):
+    """Convert a snailfish number from a string to a list with integers."""
+    return eval(sn)
+
+
+def reduce(sn):
+    """Reduce a snailfish number until no further reduction is possible.
+
+    To reduce a snailfish number, you must repeatedly do the first action
+    in this list that applies to the snailfish number:
+
+    - If any pair is nested inside four pairs, the leftmost such pair explodes.
+    - If any regular number is 10 or greater, the leftmost such regular number splits.
+    """
+    changed = True
+    while changed:
+        changed = False
+        # parse from the left and count opening paranthesis and check for any numbers > 10
+        new_sn = []
+        open_par_count = 0
+        # last integer encountered, position encountered
+        last_int_left = (None, None)
+
+    return ''
+
 # @aoc_timer
+
+
 def part1(puzzle_input):
     """Solve part 1. Return the required output value."""
 
@@ -45,5 +77,5 @@ if __name__ == '__main__':
     p2 = part2(puzzle_input)
     print(f'Part 2: {p2}')
 
-# Part 1: Start:  End:
+# Part 1: Start: 18:05 End:
 # Part 2: Start:  End:
--- 2021/tests/test_aoc2021_18.py
+++ 2021/tests/test_aoc2021_18.py
@@ -1,7 +1,7 @@
 """Test the examples given in the puzzle to verify the solution is working."""
 
 # load the required functions from the actual solution
-from solutions.aoc2021_18 import load_input, part1, part2
+from solutions.aoc2021_18 import load_input, part1, part2, reduce, add, convert
 
 
 class Test_AOC2021_18:
@@ -13,9 +13,31 @@ class Test_AOC2021_18:
     Tests can then be run in the day's directory with `pytest`.
     """
 
-    def test_1_1(self):
-        puzzle_input = load_input('testinput/18_1_1.txt')
-        assert part1(puzzle_input) == 1
+    def test_1_convert_1(self):
+        puzzle_input = '[[[[[9,8],1],2],3],4]'
+        assert convert(puzzle_input) == [[[[[9, 8], 1], 2], 3], 4]
+
+    def test_1_add_1(self):
+        a, b = '[[[[4,3],4],4],[7,[[8,4],9]]]', '[1,1]'
+        sn_a = convert(a)
+        sn_b = convert(b)
+        assert add(sn_a, sn_b) == [
+            [[[[4, 3], 4], 4], [7, [[8, 4], 9]]], [1, 1]]
+
+    def test_1_reduction_1(self):
+        puzzle_input = '[[[[[9,8],1],2],3],4]'
+        assert reduce(
+            puzzle_input) == '[[[[0,9],2],3],4]'
+
+    def test_1_reduction_2(self):
+        puzzle_input = '[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]'
+        assert reduce(
+            puzzle_input) == '[[[[0,7],4],[[7,8],[6,0]]],[8,1]]'
+
+    # def test_1_1(self):
+    #     puzzle_input = load_input('testinput/18_1_1.txt')
+    #     assert part1(
+    #         puzzle_input) == '[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]'
 
     # def test_2_1(self):
     #     puzzle_input = load_input('testinput/18_1_1.txt')
