24708db430e81a499080fe3180f1d4a93bd8d830
Bjoern Winkler <bjoern@bjoern-winkler.de>
2021-12-26 13:49:45 +0100

    2021 day 17 pt 1 wip

--- 2021/solutions/aoc2021_17.py
+++ 2021/solutions/aoc2021_17.py
@@ -20,59 +20,32 @@ def load_input(f_name):
     return puzzle_input
 
 
-def calc_velocity(x, y, target):
-    """Calculates the velocity required to hit an (x, y) coordinate in n steps.
-
-    Formular used is derived from
-
-    x = sum(vx + i * dx), y = sum(vy + i * dy) for i=0..n, which can be resolved to vx and vy:
-    vx = (x + sum(i * dx)) // n = (2x + (n - 1) * n) // 2n
-    vy = (y + sum(i * dy)) // n = (2y + (n - 1) * n) // 2n
-
-    However, for vx the decreases stop at 0, so we are really only summing up to vx and not n.
-    Hence the actual formula for x = sum(vx + i * dx) for i=0..vx can be resolved to
-    2x = vx^2 * (vx + 1).
-
-    Using the solution to the quadratic formula (ax^2 + bx + c = 0, x = (-b +/- sqrt(b^2 - 4ac)) / 2a), 
-    we can replace a and b with 1 and c with -2x to calculate vx:
-    vx = (-1 + sqrt(1 - 4 * -2x)) / 2
-    """
-    results = []
-    max_steps = max(abs(a) for a in [x, y])
-    for n in range(1, max_steps + 1):
-        vx, rem_x = divmod((-1 + round(math.sqrt(1 + 8 * x))), 2)
-        vy, rem_y = divmod((2 * y + (n - 1) * n), (2 * n))
-        if rem_x == 0 and rem_y == 0:
-            print(f'x/y/n: {x}, {y}, {n}: {vx}, {vy}')
-            if is_in_target(vx, vy, n, target):
-                results.append((vx, vy, n))
-    return results
-
-
-def is_in_target(vx, vy, n, target):
-    """Calculate if a pair ends up within the target (min_x, max_x, min_y, max_y)."""
-    steps_x = min(vx, n)
-    steps_y = n
-    x = steps_x * vx - ((steps_x - 1) * steps_x) // 2
-    y = steps_y * vy - ((steps_y - 1) * steps_y) // 2
-    print(f'{x=}, {y=}, {vx=}, {vy=}, {n=}')
-    return target[0] <= x <= target[1] and target[2] <= y <= target[3]
-
-# @aoc_timer
-
-
 def part1(puzzle_input):
     """Solve part 1. Return the required output value."""
 
     min_x, max_x, min_y, max_y = puzzle_input
-    max_steps = max(abs(x) for x in puzzle_input)
-    valid_velocities = []
-    for y in range(min_y, max_y + 1):
-        for x in range(min_x, max_x + 1):
-            results = calc_velocity(x, y, puzzle_input)
-            valid_velocities.extend(results)
+    valid_vx = []
+    valid_velocity = []
+    for vx in range(1, max_x):
+        temp_vx = vx
+        n = 0
+        x = 0
+        while temp_vx > 0:
+            x += temp_vx
+            temp_vx -= 1
+            n += 1
+            if min_x <= x <= max_x:
+                for ny in range(n, 10 * n):
+                    for vy in range(10 * min_y, 10 * abs(min_y)):
+                        sumny, rem_y = divmod((ny-1) * ny, 2)
+                        if rem_y == 0:
+                            y = ny * vy - sumny
+                            # TODO: Need a good way to calculate x with vx - this takes any y values that fit in.
+                            if min_y <= y <= max_y and min_x <= x <= max_x:
+                                valid_velocity.append((vx, vy, ny))
 
-    print(set(valid_velocities))
+    print(valid_velocity)
+
     return 1
 
 
