b619cd2921e76e840a743cc51919fc546c62bb77
Bjoern Winkler <bjoern@bjoern-winkler.de>
2021-12-29 19:53:40 +0100

    2021 day 20 done

--- 2021/solutions/aoc2021_20.py
+++ 2021/solutions/aoc2021_20.py
@@ -3,13 +3,13 @@
 # import re
 from collections import defaultdict
 from itertools import product
-# from utils.aoctools import aoc_timer
+from utils.aoctools import aoc_timer
 
 
 def load_input(f_name):
-    """Loads the puzzle input from the specified file. 
+    """Loads the puzzle input from the specified file.
 
-    Specify the relative path if loading files from a subdirectory, 
+    Specify the relative path if loading files from a subdirectory,
     e.g. for loading test inputs, specify `testinput/01_1_1.txt`.
 
     Convert # into 1 and . into 0s.
@@ -17,7 +17,7 @@ def load_input(f_name):
     with open(f_name, 'r') as f:
         algo, bottom = f.read().split('\n\n')
         algo = [1 if x == '#' else 0 for x in algo.strip()]
-        image = defaultdict(int)
+        image = {}
         for r, line in enumerate(bottom.split('\n')):
             for c, x in enumerate(line.strip()):
                 image[(r, c)] = 1 if x == '#' else 0
@@ -33,13 +33,13 @@ def print_image(image: defaultdict[tuple[int], int]):
     for r in range(min_r, max_r + 1):
         line = ''
         for c in range(min_c, max_c + 1):
-            line += '#' if image[(r, c)] == 1 else '.'
+            line += '#' if image.get((r, c), 0) == 1 else '.'
         print(line)
     print()
 
 
 def neighbors(r: int, c: int) -> list[tuple[int]]:
-    """Calculate 9 neighboring pixels in (r, c) notation. 
+    """Calculate 9 neighboring pixels in (r, c) notation.
     Includes the input (r, c) pixel.
     """
     offsets = [-1, 0, 1]
@@ -50,12 +50,12 @@ def neighbors(r: int, c: int) -> list[tuple[int]]:
 
 
 def img_dimensions(image: defaultdict[tuple[int], int]) -> list[tuple[int]]:
-    """Calculate the dimensions of the visible image by taking the 
+    """Calculate the dimensions of the visible image by taking the
     space occupied by "lit" pixels (== 1 value).
 
     Adds padding of 1 pixel to each side.
     """
-    padding = 5
+    padding = 1
     lit_pixel_coords = [x for x in image if image[x] == 1]
     min_r = min(lit_pixel_coords, key=lambda x: x[0])[0] - padding
     max_r = max(lit_pixel_coords, key=lambda x: x[0])[0] + padding
@@ -65,17 +65,32 @@ def img_dimensions(image: defaultdict[tuple[int], int]) -> list[tuple[int]]:
     return [min_r, max_r, min_c, max_c]
 
 
-def apply_algo(image: defaultdict[tuple[int], int], algo: list[int]) -> defaultdict[tuple[int], int]:
-    """Apply image enhancement algorithm to the provided image once."""
+def apply_algo(image: defaultdict[tuple[int], int], algo: list[int], cycle: int) -> defaultdict[tuple[int], int]:
+    """Apply image enhancement algorithm to the provided image once.
+
+    Depending how many cycles are being done of the algorithm, the default pixel value
+    switches from dark (on even number of cycles, starting with 0) to light (on uneven
+    number of cycles, starting with 1).
+    """
     # determine image dimensions - need to consider frame of 1 pixel
     # outside of the widest pixels
     min_r, max_r, min_c, max_c = img_dimensions(image)
+    # alternate the default pixel in infinity between the algo[0] (for odd cycles)
+    # and what algo[9 * algo[0]] yields - which is what any infinite pixels get flipped to
+    # on the next cycle.
+    even = algo[int(
+                ''.join(str(algo[0]) * 9),
+                base=2
+                )]
+    odd = algo[0]
 
-    new_image = defaultdict(int)
+    # on even number of cycles, default pixel is 0, on others it is 1
+    new_image = {}
     for r in range(min_r, max_r + 1):
         for c in range(min_c, max_c + 1):
             i = int(
-                ''.join(str(image[(nr, nc)]) for nr, nc in neighbors(r, c)),
+                ''.join(str(image.get((nr, nc), default_pixel(cycle, even, odd)))
+                        for nr, nc in neighbors(r, c)),
                 base=2
             )
             new_image[(r, c)] = algo[i]
@@ -83,23 +98,31 @@ def apply_algo(image: defaultdict[tuple[int], int], algo: list[int]) -> defaultd
     return new_image
 
 
+def default_pixel(cycle, even, odd):
+    """Returns the default pixel value - 0 for even cycles (default pixel dark),
+    1 for uneven cycles (default pixel lit).
+    """
+    return even if cycle % 2 == 0 else odd
+
+
 def lit_pixels(image: defaultdict[tuple[int], int]) -> int:
     """Calculate the number of lit pixels in an image."""
     return sum(image[coord] for coord in image)
 
 
-def part1(puzzle_input):
+@aoc_timer
+def part1(puzzle_input, cycles=2):
     """Solve part 1. Return the required output value."""
 
     algo, image = puzzle_input
 
-    print(f'Original image: {lit_pixels(image)} pixels lit.')
-    print(f'Dimensions: {img_dimensions(image)}')
+    # print(f'Original image: {lit_pixels(image)} pixels lit.')
+    # print(f'Dimensions: {img_dimensions(image)}')
 
-    for i in range(2):
-        image = apply_algo(image, algo)
-        print(f'Algo pass {i}: {lit_pixels(image)} pixels lit.')
-        print(f'Dimensions: {img_dimensions(image)}')
+    for i in range(cycles):
+        image = apply_algo(image, algo, i)
+        # print(f'Algo pass {i}: {lit_pixels(image)} pixels lit.')
+        # print(f'Dimensions: {img_dimensions(image)}')
 
     return lit_pixels(image)
 
@@ -120,8 +143,13 @@ if __name__ == '__main__':
     print(f'Part 1: {p1}')
 
     # Solve part 2 and print the answer
-    p2 = part2(puzzle_input)
+    p2 = part1(puzzle_input, 50)
     print(f'Part 2: {p2}')
 
-# Part 1: Start: 17:23 End:
-# Part 2: Start:  End:
+# Part 1: Start: 17:23 End: 19:30
+# Part 2: Start: 19:31 End: 19:41
+
+# Elapsed time to run part1: 0.10244 seconds.
+# Part 1: 5400
+# Elapsed time to run part1: 5.54760 seconds.
+# Part 2: 18989
