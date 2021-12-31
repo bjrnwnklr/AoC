4750ff6f82c210907304cc51f0cefb8016a099fa
Bjoern Winkler <bjoern@bjoern-winkler.de>
2021-12-31 18:33:18 +0100

    2021 day 21 pt1 done

--- 2021/solutions/aoc2021_21.py
+++ 2021/solutions/aoc2021_21.py
@@ -14,16 +14,52 @@ def load_input(f_name):
     with open(f_name, 'r') as f:
         puzzle_input = []
         for line in f.readlines():
-            puzzle_input.append(line.strip())
+            puzzle_input.append(int((line.strip().split(':'))[1]))
 
     return puzzle_input
 
 
+def deterministic_dice(n: int = 100) -> list[int]:
+    """Generator: deterministic dice. Delivers sum of (1+2+3, 4+5+6, ...), 
+    wrapping around at n (default 100)
+    """
+    i = 0
+    while True:
+        s = 0
+        for _ in range(3):
+            i = (i % n) + 1
+            s += i
+        yield s
+
 # @aoc_timer
+
+
 def part1(puzzle_input):
     """Solve part 1. Return the required output value."""
 
-    return 1
+    print()
+
+    p1 = puzzle_input[0]
+    p2 = puzzle_input[1]
+    s1 = s2 = 0
+    d = deterministic_dice(100)
+    cycles = 0
+    while s1 < 1000 and s2 < 1000:
+        n = next(d)
+        p1 = (p1 + n - 1) % 10 + 1
+        s1 += p1
+        # print(f'{n=} - {p1= }: {s1=}')
+        cycles += 3
+        if s1 >= 1000:
+            break
+        n = next(d)
+        p2 = (p2 + n - 1) % 10 + 1
+        s2 += p2
+        # print(f'{n=} - {p2=}: {s2=}')
+        cycles += 3
+
+    print(f'{cycles=}, {s1=}, {s2=}')
+    return min(s1, s2) * cycles
 
 
 # @aoc_timer
@@ -45,5 +81,5 @@ if __name__ == '__main__':
     p2 = part2(puzzle_input)
     print(f'Part 2: {p2}')
 
-# Part 1: Start:  End:
+# Part 1: Start: 17:39 End:
 # Part 2: Start:  End:
