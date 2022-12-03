1190438b6b5d462458be607d03fc51f51a1806d8
Bjoern Winkler <bjoern@bjoern-winkler.de>
2022-12-03 18:50:27 +0100

    2022 day 2 part 2 solved

--- 2022/solutions/aoc2022_02.py
+++ 2022/solutions/aoc2022_02.py
@@ -20,13 +20,18 @@ def load_input(f_name):
 
 
 RPS = {"A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3}
+RPS2 = {"A": 1, "B": 2, "C": 3, "X": 0, "Y": 3, "Z": 6}
 
 
-def translate_rps(line):
+def translate_rps(line, part=1):
     """Translates an input line 'A Y' into numbers
     (1, 2)"""
     l, r = line.split()
-    return (RPS[l], RPS[r])
+    if part == 1:
+        d = RPS
+    else:
+        d = RPS2
+    return (d[l], d[r])
 
 
 # @aoc_timer
@@ -73,9 +78,31 @@ def part1(puzzle_input):
 
 # @aoc_timer
 def part2(puzzle_input):
-    """Solve part 2. Return the required output value."""
+    """Solve part 2. Return the required output value.
 
-    return 1
+    X lose 0
+    Y draw 3
+    Z win  6
+    """
+    score = 0
+    for line in puzzle_input:
+        l, r = translate_rps(line, 2)
+        # add win / draw / loss to score
+        score += r
+        match r:
+            case 0:
+                # loss, add 2 to l
+                result = ((l + 1) % 3) + 1
+                score += result
+            case 3:
+                # draw, just add
+                score += l
+            case 6:
+                # win, add 1 to l
+                result = (l % 3) + 1
+                score += result
+
+    return score
 
 
 if __name__ == "__main__":
@@ -91,4 +118,4 @@ if __name__ == "__main__":
     print(f"Part 2: {p2}")
 
 # Part 1: Start: 17:38 End: 18:02
-# Part 2: Start: 18:03 End:
+# Part 2: Start: 18:03 End: 18:50
