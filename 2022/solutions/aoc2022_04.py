5f9db804ee515c071e59352f78ceca7102250c10
Bjoern Winkler <bjoern@bjoern-winkler.de>
2022-12-04 15:06:34 +0100

    2022 day 4 part 1 solved

--- 2022/solutions/aoc2022_04.py
+++ 2022/solutions/aoc2022_04.py
@@ -1,6 +1,7 @@
 # Load any required modules. Most commonly used:
 
-# import re
+import re
+
 # from collections import defaultdict
 # from utils.aoctools import aoc_timer
 
@@ -11,19 +12,30 @@ def load_input(f_name):
     Specify the relative path if loading files from a subdirectory,
     e.g. for loading test inputs, specify `testinput/01_1_1.txt`.
     """
+    regex = r"(\d+)"
     with open(f_name, "r") as f:
         puzzle_input = []
         for line in f.readlines():
-            puzzle_input.append(line.strip())
+            matches = re.findall(regex, line.strip())
+            if matches:
+                puzzle_input.append(list(map(int, matches)))
 
     return puzzle_input
 
 
 # @aoc_timer
 def part1(puzzle_input):
-    """Solve part 1. Return the required output value."""
+    """Solve part 1. Return the required output value.
 
-    return 1
+    In how many assignment pairs does one range fully contain the other?
+    """
+    result = 0
+    for line in puzzle_input:
+        l1, r1, l2, r2 = line
+        if (l1 >= l2 and r1 <= r2) or (l2 >= l1 and r2 <= r1):
+            result += 1
+
+    return result
 
 
 # @aoc_timer
@@ -45,5 +57,5 @@ if __name__ == "__main__":
     p2 = part2(puzzle_input)
     print(f"Part 2: {p2}")
 
-# Part 1: Start:  End:
-# Part 2: Start:  End:
+# Part 1: Start: 14:55 End: 15:06
+# Part 2: Start: 15:07 End:
--- 2022/tests/test_aoc2022_04.py
+++ 2022/tests/test_aoc2022_04.py
@@ -16,7 +16,7 @@ class Test_AOC2022_04:
 
     def test_1_1(self):
         puzzle_input = load_input("testinput/04_1_1.txt")
-        assert part1(puzzle_input) == 1
+        assert part1(puzzle_input) == 2
 
     # def test_2_1(self):
     #     puzzle_input = load_input('testinput/04_1_1.txt')
