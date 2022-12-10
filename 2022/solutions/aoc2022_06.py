2a5cc21e55c64c29aea2360844c73ab454aa1d41
Bjoern Winkler <bjoern@bjoern-winkler.de>
2022-12-10 15:04:34 +0100

    downloaded days 5-10

---
+++ 2022/solutions/aoc2022_06.py
@@ -0,0 +1,65 @@
+# Load any required modules. Most commonly used:
+
+# import re
+# from collections import defaultdict
+# from utils.aoctools import aoc_timer
+
+
+def load_input(f_name):
+    """Loads the puzzle input from the specified file.
+
+    Specify the relative path if loading files from a subdirectory,
+    e.g. for loading test inputs, specify `testinput/01_1_1.txt`.
+    """
+    # return input as list of text lines
+    with open(f_name, "r") as f:
+        puzzle_input = []
+        for line in f.readlines():
+            puzzle_input.append(line.strip())
+
+    # Extract ints from the input
+    #
+    # signed ints
+    # regex = re.compile(r"(-?\d+)")
+    #
+    # unsigned ints
+    # regex = re.compile(r"(\d+)")
+    #
+    # with open(f_name, "r") as f:
+    #     puzzle_input = []
+    #     for line in f.readlines():
+    #         matches = regex.findall(line.strip())
+    #         if matches:
+    #             puzzle_input.append(list(map(int, matches)))
+
+    return puzzle_input
+
+
+# @aoc_timer
+def part1(puzzle_input):
+    """Solve part 1. Return the required output value."""
+
+    return 1
+
+
+# @aoc_timer
+def part2(puzzle_input):
+    """Solve part 2. Return the required output value."""
+
+    return 1
+
+
+if __name__ == "__main__":
+    # read the puzzle input
+    puzzle_input = load_input("input/06.txt")
+
+    # Solve part 1 and print the answer
+    p1 = part1(puzzle_input)
+    print(f"Part 1: {p1}")
+
+    # Solve part 2 and print the answer
+    p2 = part2(puzzle_input)
+    print(f"Part 2: {p2}")
+
+# Part 1: Start:  End:
+# Part 2: Start:  End:
