66d17b09aa87b5b28075160aa94ea84432999716
Bjoern Winkler <bjoern@bjoern-winkler.de>
2022-12-11 15:39:00 +0100

    2022 day 7 part 1 wip

--- 2022/solutions/aoc2022_07.py
+++ 2022/solutions/aoc2022_07.py
@@ -1,7 +1,7 @@
 # Load any required modules. Most commonly used:
 
 # import re
-# from collections import defaultdict
+from collections import defaultdict
 # from utils.aoctools import aoc_timer
 
 
@@ -34,12 +34,81 @@ def load_input(f_name):
 
     return puzzle_input
 
+class Node:
+    def __init__(self, name, parent) -> None:
+        self.name = name
+        self.parent = parent
+        self.children = {}
+        self.size = 0
+        self.total_size = 0
+
+    def __repr__(self) -> str:
+        return f'[{self.name} p: {self.parent} s: {self.size} ts: {self.total_size} c: {self.children}]'
+
+    def inc_size(self, s):
+        """Add s to size and all parent directories"""
+        self.total_size += s
+        if self.parent:
+            self.parent.inc_size(s)
+
+def add_up(node: Node, threshold: int = 100_000) -> int:
+    """Sums up the total sizes of each node if <= threshold, recursively"""
+    
+    inc = node.total_size if node.total_size <= threshold else 0
+    print(f'Adding {node.name} size: {node.total_size}, inc: {inc}')
+    return inc + sum(add_up(n) for n in node.children.values())
+
 
 # @aoc_timer
 def part1(puzzle_input):
-    """Solve part 1. Return the required output value."""
+    """Solve part 1. Return the required output value.
+    
+    Find all directories with size <= 100_000; what is
+    the sum of the total sizes of those directories?
 
-    return 1
+    Directories contained in other directories count
+    multiple times.
+    """
+    # store Node elements for each directory
+    dir_tree = {}
+    # create root node and store in tree
+    curr_node = Node('/', None)
+    dir_tree['/'] = curr_node
+
+    for line in puzzle_input:
+        match line.split():
+            case ['$', 'cd', d] if d == '..':
+                # go up one directory
+                curr_node = curr_node.parent
+                print(f'Going up one dir. Current node: {curr_node.name}')
+            case ['$', 'cd', d]:
+                # add dir to curr_dir list
+                curr_node = dir_tree[d]
+                print(f'Changing to dir {d}. Current node: {curr_node.name}')
+            case ['$', 'ls']:
+                # we don't need to do anything, wait for file name
+                pass
+            case ['dir', d]:
+                # found a directory, create a new node with current node as parent
+                # check that we do not try to create a dir that already exists
+                assert d not in dir_tree
+                
+                # add to children of current node
+                new_dir = Node(d, curr_node)
+                curr_node.children[d] = new_dir
+                dir_tree[d] = new_dir
+                print(f'New directory {d}. Current node: {curr_node.name}')
+            case [size, name]:
+                # found a file with size and name
+                curr_node.inc_size(int(size))
+                print(f'File {name} found with size {size}. Current node: {curr_node.name}')
+
+    # find all directories with total_size <= 100_000
+    print(dir_tree['/'])
+    result = add_up(dir_tree['/'])
+    print(f'Result: {result}')
+
+    return result
 
 
 # @aoc_timer
@@ -61,5 +130,5 @@ if __name__ == "__main__":
     p2 = part2(puzzle_input)
     print(f"Part 2: {p2}")
 
-# Part 1: Start:  End:
+# Part 1: Start: 14:23 End:
 # Part 2: Start:  End:
