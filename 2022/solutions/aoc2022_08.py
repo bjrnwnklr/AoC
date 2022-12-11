a231ff896d034354792e2fcb7ae3b2fe07240a0a
Bjoern Winkler <bjoern@bjoern-winkler.de>
2022-12-11 18:04:31 +0100

    2022 day 8 part 1 done

--- 2022/solutions/aoc2022_08.py
+++ 2022/solutions/aoc2022_08.py
@@ -15,31 +15,46 @@ def load_input(f_name):
     with open(f_name, "r") as f:
         puzzle_input = []
         for line in f.readlines():
-            puzzle_input.append(line.strip())
-
-    # Extract ints from the input
-    #
-    # signed ints
-    # regex = re.compile(r"(-?\d+)")
-    #
-    # unsigned ints
-    # regex = re.compile(r"(\d+)")
-    #
-    # with open(f_name, "r") as f:
-    #     puzzle_input = []
-    #     for line in f.readlines():
-    #         matches = regex.findall(line.strip())
-    #         if matches:
-    #             puzzle_input.append(list(map(int, matches)))
+            puzzle_input.append(list(map(int, list(line.strip()))))
 
     return puzzle_input
+
+
+def visible(grid, r, c) -> bool:
+    """Return if a tree at given coordinates is visible from anywhere."""
+    # row and column of the tree
+    row = grid[r]
+    col = [line[c] for line in grid]
+
+    # height of the tree
+    height = grid[r][c]
+    # segments of row and col to left, right, top, bottom of tree
+    left = row[:c]
+    right = row[c + 1 :]
+    top = col[:r]
+    bottom = col[r + 1 :]
+
+    # tree is visible if the highest tree on any side is lower than tree's height
+    return any(max(side) < height for side in [left, right, top, bottom])
 
 
 # @aoc_timer
 def part1(puzzle_input):
     """Solve part 1. Return the required output value."""
+    # count trees on the side
+    height = len(puzzle_input)
+    width = len(puzzle_input[0])
 
-    return 1
+    # count of visible trees (subtract 4 corners as double counted)
+    count_visible = 2 * height + 2 * width - 4
+
+    # count all visible trees in the middle
+    for r in range(1, height - 1):
+        for c in range(1, width - 1):
+            if visible(puzzle_input, r, c):
+                count_visible += 1
+
+    return count_visible
 
 
 # @aoc_timer
@@ -61,5 +76,5 @@ if __name__ == "__main__":
     p2 = part2(puzzle_input)
     print(f"Part 2: {p2}")
 
-# Part 1: Start:  End:
-# Part 2: Start:  End:
+# Part 1: Start: 17:42 End: 18:04
+# Part 2: Start: 18:05 End:
--- 2022/tests/test_aoc2022_08.py
+++ 2022/tests/test_aoc2022_08.py
@@ -16,7 +16,7 @@ class Test_AOC2022_08:
 
     def test_1_1(self):
         puzzle_input = load_input("testinput/08_1_1.txt")
-        assert part1(puzzle_input) == 1
+        assert part1(puzzle_input) == 21
 
     # def test_2_1(self):
     #     puzzle_input = load_input('testinput/08_1_1.txt')
