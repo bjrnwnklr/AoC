de754745154db6a8189c0bce230ddee175da2682
Bjoern Winkler <bjoern@bjoern-winkler.de>
2022-12-16 19:09:09 +0100

    2022 day 9 part 1 solved

--- 2022/solutions/aoc2022_09.py
+++ 2022/solutions/aoc2022_09.py
@@ -16,44 +16,60 @@ def load_input(f_name):
     with open(f_name, "r") as f:
         puzzle_input = []
         for line in f.readlines():
-            puzzle_input.append(line.strip())
-
-    # Extract ints from the input
-    #
-    # signed ints
-    # regex = re.compile(r"(-?\d+)")
-    #
-    # unsigned ints
-    # regex = re.compile(r"(\d+)")
-    #
-    # with open(f_name, "r") as f:
-    #     puzzle_input = []
-    #     for line in f.readlines():
-    #         matches = regex.findall(line.strip())
-    #         if matches:
-    #             puzzle_input.append(list(map(int, matches)))
+            d, n = line.strip().split()
+            puzzle_input.append((d, int(n)))
 
     return puzzle_input
 
 
 @dataclass
-class Rope:
-    head: tuple(int) = (0, 0)
-    tail: tuple(int) = (0, 0)
-
-    def is_tail_move(self) -> bool:
-        """Calculate if tail moves.
-        Tail moves if head is not adjacent to tail.
-        """
-        return (self.head[0] - 1 <= self.tail[0] <= self.head[0] + 1) and (
-            self.head[1] - 1 <= self.tail[1] <= self.head[1] + 1
-        )
+class Knot:
+    x: int = 0
+    y: int = 0
 
 
-def move(rope: Rope, dir, count):
-    """Move the provided rope into direction dir by count steps."""
-    # TODO: Create move logic for moving the head / tail
-    pass
+DIRECTIONS = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
+
+
+def move(direction, knot):
+    """Move a knot one step into direction"""
+    knot.x += direction[0]
+    knot.y += direction[1]
+
+
+def sign(n: int):
+    """Return the sign of the number."""
+    if n == 0:
+        return 0
+    elif n > 0:
+        return 1
+    else:
+        return -1
+
+
+def distance(head, tail):
+    """Return the distance between head and tail for each
+    coordinate."""
+    return (head.x - tail.x, head.y - tail.y)
+
+
+def one_move(direction, head, tail):
+    """Move both head and tail by one step into direction."""
+    # move the head
+    move(DIRECTIONS[direction], head)
+    # print(f"\tHead: {head}")
+
+    # determine if tail needs to move
+    # - if distance in x or y is > 2, tail needs to move
+    # - tail moves by (sign(delta.x), sign(delta.y)),
+    # i.e. if delta.x == 0: no move
+    #      if delta.x > 0: move by 1
+    #      if delta.x < 0: move by -1
+    d = distance(head, tail)
+    if abs(d[0]) > 1 or abs(d[1]) > 1:
+        # move tail
+        move((sign(d[0]), sign(d[1])), tail)
+        # print(f"\tTail moved: {tail}")
 
 
 # @aoc_timer
@@ -64,11 +80,17 @@ def part1(puzzle_input):
     This includes the starting position
     """
     tail_visits = set()
-    head = (0, 0)
-    tail = (0, 0)
-    print(f"bla   ")
+    head = Knot()
+    tail = Knot()
 
-    return 1
+    for direction, steps in puzzle_input:
+        # print(f"Motion: {direction=}, {steps=}")
+        for _ in range(steps):
+            one_move(direction, head, tail)
+            tail_visits.add((tail.x, tail.y))
+            # print(f"\t{tail_visits=}")
+
+    return len(tail_visits)
 
 
 # @aoc_timer
@@ -90,5 +112,5 @@ if __name__ == "__main__":
     p2 = part2(puzzle_input)
     print(f"Part 2: {p2}")
 
-# Part 1: Start: 18:42 End:
-# Part 2: Start:  End:
+# Part 1: Start: 18:42 End: 19:08
+# Part 2: Start: 19:09 End:
