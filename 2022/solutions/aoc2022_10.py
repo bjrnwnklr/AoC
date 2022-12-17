fe0ce45f1312182c939ed041d766d0346399524e
Bjoern Winkler <bjoern@bjoern-winkler.de>
2022-12-17 11:50:50 +0100

    2022 day 10 part 1

--- 2022/solutions/aoc2022_10.py
+++ 2022/solutions/aoc2022_10.py
@@ -3,6 +3,7 @@
 # import re
 # from collections import defaultdict
 # from utils.aoctools import aoc_timer
+from dataclasses import dataclass
 
 
 def load_input(f_name):
@@ -17,29 +18,55 @@ def load_input(f_name):
         for line in f.readlines():
             puzzle_input.append(line.strip())
 
-    # Extract ints from the input
-    #
-    # signed ints
-    # regex = re.compile(r"(-?\d+)")
-    #
-    # unsigned ints
-    # regex = re.compile(r"(\d+)")
-    #
-    # with open(f_name, "r") as f:
-    #     puzzle_input = []
-    #     for line in f.readlines():
-    #         matches = regex.findall(line.strip())
-    #         if matches:
-    #             puzzle_input.append(list(map(int, matches)))
-
     return puzzle_input
 
 
+@dataclass
+class CPU:
+    X: int = 1
+    cycle: int = 0
+    signal_strength: int = 0
+
+    def update_signal_strength(self):
+        if self.cycle in CYCLES:
+            self.signal_strength += self.X * self.cycle
+            # print(f'{self}')
+
+CYCLES = [20, 60, 100, 140, 180, 220]
+
 # @aoc_timer
 def part1(puzzle_input):
-    """Solve part 1. Return the required output value."""
+    """Solve part 1. Return the required output value.
 
-    return 1
+    Find signal strength (X value * cycle) at
+    - 20
+    - 60
+    - 100
+    - 140
+    - 180
+    - 220 cycles
+    Sum up for result
+    """
+    cpu = CPU()
+
+    for line in puzzle_input:
+        # print(f'[{cpu.cycle}] {line} ({cpu})')
+        match line.split():
+            case ["noop"]:
+                cpu.cycle += 1
+                # evaluate if we reached a cycle point
+                cpu.update_signal_strength()
+
+            case ["addx", val]:
+                cpu.cycle += 1
+                cpu.update_signal_strength()
+                cpu.cycle += 1
+                cpu.update_signal_strength()
+                # x is only increased AFTER the cycle
+                cpu.X += int(val)
+
+
+    return cpu.signal_strength
 
 
 # @aoc_timer
@@ -61,5 +88,5 @@ if __name__ == "__main__":
     p2 = part2(puzzle_input)
     print(f"Part 2: {p2}")
 
-# Part 1: Start:  End:
-# Part 2: Start:  End:
+# Part 1: Start: 11:25 End: 11:50
+# Part 2: Start: 11:51 End:
