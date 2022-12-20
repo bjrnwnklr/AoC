446b0dc7a4efc2e295b61554be1019bea8118dc7
Bjoern Winkler <bjoern@bjoern-winkler.de>
2022-12-20 07:02:39 +0100

    hackerrank mud optimized with queue

--- other/hackerrank/mud.py
+++ other/hackerrank/mud.py
@@ -1,5 +1,4 @@
 def solve(wallPositions, wallHeights):
-    print("Starting ---")
 
     last_wall = wallPositions[-1]
     all_wall_heights = {p: h for p, h in zip(wallPositions, wallHeights)}
@@ -7,49 +6,46 @@ def solve(wallPositions, wallHeights):
 
     # iterate through each wall position
     for i in range(1, last_wall + 1):
-        print(f"Checking wall: {i}")
         # check if brick wall
         if i in all_wall_heights:
-            print(f"Wall found: {i}")
+            # we treat wallPositions as a queue and pop off an element once we find it
+            assert i == wallPositions[0]
+            wallPositions.pop(0)
             continue
         # otherwise, we need to build a mudwall
         # get left wall position (this is simply current - 1)
         left = i - 1
-        # get rigth wall position (count up until we find it)
-        j = 1
-        while (i + j) not in all_wall_heights:
-            j += 1
-        right = i + j
-        print(f"Found left and right wall: {left} {right}")
+        # right wall is the first element in the wallPositions queue
+        right = wallPositions[0]
 
         # get gap between walls
         wall_gap = right - left
         # get height gap between walls
         height_gap = all_wall_heights[right] - all_wall_heights[left]
-        print(f"{wall_gap=}, {height_gap=}")
 
         # if height gap is 0 or positive, build up
         if height_gap >= 0:
             all_wall_heights[i] = all_wall_heights[left] + 1
-            print(f"Building up: {i}: {all_wall_heights[i]}")
 
         # if height gap is negative, determine if we
         # - build up: wall_gap > abs(height_gap) + 1
         # - stay: wall_gap = abs(height_gap) + 1 (exactly one step above)
         # - build down: wall_gap <= abs(height_gap)
         else:
-            if wall_gap > abs(height_gap) + 1:
+            wall_difference = wall_gap - abs(height_gap)
+            if wall_difference > 1:
                 delta = 1
-            elif wall_gap == abs(height_gap) + 1:
+            elif wall_difference == 1:
                 delta = 0
             else:
                 delta = -1
             all_wall_heights[i] = all_wall_heights[left] + delta
-            print(f"Adding delta: {delta}, {i}: {all_wall_heights[i]}")
 
         # update max_height
         max_height = max(max_height, all_wall_heights[i])
+
     print(f"Max height = {max_height}")
+
     return max_height
 
 
