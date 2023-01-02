2c4dee6cca56a9bd4c1a7e31728796c5b5b19d39
Bjoern Winkler <bjoern@bjoern-winkler.de>
2023-01-02 18:17:22 +0100

    2022 day 17 part 1 test case working

--- 2022/solutions/aoc2022_17.py
+++ 2022/solutions/aoc2022_17.py
@@ -3,6 +3,10 @@
 # import re
 # from collections import defaultdict
 # from utils.aoctools import aoc_timer
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
 
 
 def load_input(f_name):
@@ -14,32 +18,137 @@ def load_input(f_name):
     # return input as list of text lines
     with open(f_name, "r") as f:
         puzzle_input = []
-        for line in f.readlines():
-            puzzle_input.append(line.strip())
-
-    # Extract ints from the input
-    #
-    # signed ints
-    # regex = re.compile(r"(-?\d+)")
-    #
-    # unsigned ints
-    # regex = re.compile(r"(\d+)")
-    #
-    # with open(f_name, "r") as f:
-    #     puzzle_input = []
-    #     for line in f.readlines():
-    #         matches = regex.findall(line.strip())
-    #         if matches:
-    #             puzzle_input.append(list(map(int, matches)))
+        line = f.readline()
+        puzzle_input = list(line.strip())
 
     return puzzle_input
 
 
+class Shape:
+
+    # shapes are described by the relative coordinates of the filled parts of the shape
+    # to an anchor (e.g. bottom left corner)
+    # (e.g. the plus) in (r, c) notation
+
+    rocks = {
+        0: [(0, 0), (0, 1), (0, 2), (0, 3)],  # -
+        1: [(0, 1), (-1, 0), (-1, 1), (-1, 2), (-2, 1)],  # +
+        2: [(0, 0), (0, 1), (0, 2), (-1, 2), (-2, 2)],  # reverse L
+        3: [(0, 0), (-1, 0), (-2, 0), (-3, 0)],  # I
+        4: [(0, 0), (0, 1), (-1, 0), (-1, 1)],  # cube
+    }
+    directions = {">": (0, 1), "<": (0, -1), "v": (1, 0)}
+
+    def __init__(self, id, last_row) -> None:
+        # pick the shape based on id
+        self.coords = self.rocks[id % 5][:]
+        # move to starting position
+        for i in range(len(self.coords)):
+            self.coords[i] = (self.coords[i][0] + last_row - 4, self.coords[i][1] + 3)
+
+        logging.debug(f"Created shape: {self.coords}")
+
+    def move_coords(self, direction):
+        """Provide a list of updated coordinates after a move left,
+        right or downward."""
+        dd = self.directions[direction]
+        return [(block[0] + dd[0], block[1] + dd[1]) for block in self.coords]
+
+    def move(self, direction):
+        """Move the shape in the specified direction."""
+        self.coords = self.move_coords(direction)
+        logging.debug(f"Moved shape in direction {direction}: {self.coords}")
+
+
+class Room:
+
+    # Represents the narrow tall room
+
+    # The walls / floor are set up like this:
+    # - bottom left corner: (0, 0)
+    # - left wall: (x, 0) with x < 0
+    # - right wall: (x, 8) with x < 0
+    # - the first free row in between is (-1, 1) - (-1, 7)
+
+    def __init__(self) -> None:
+        self.grid = set()
+
+    def is_collision(self, coords) -> bool:
+        """Checks if a provided shape hits a wall, floor or other
+        stopped rock.
+
+        `coords`: list of coordinates of the shape
+        """
+        # check for collision
+        for block in coords:
+            if (
+                block in self.grid  # hit a stopped rock
+                or block[1] == 0  # hit left wall
+                or block[1] == 8  # hit right wall
+                or block[0] == 0  # hit floor
+            ):
+                return True
+
+        return False
+
+    def move(self, shape: Shape, direction: str) -> bool:
+        """Moves a shape
+        - first in the indicated direction '<' or '>'
+        - then downwards 'v'
+
+        Returns True if the downward move was successful (i.e. False if it hit
+        a stopped rock or the floor on the downward movement).
+        """
+        # sideways move
+        potential_coords = shape.move_coords(direction)
+        if not self.is_collision(potential_coords):
+            shape.move(direction)
+        # downwards move
+        potential_coords = shape.move_coords("v")
+        if not self.is_collision(potential_coords):
+            shape.move("v")
+            # if successful, return True
+            return True
+        else:
+            # hit the floor or came to rest on other rock
+            # freeze the rock and return False
+            for c in shape.coords:
+                self.grid.add(c)
+            return False
+
+    def height(self):
+        """Returns the height of the tower of rocks in the room."""
+        if self.grid:
+            return min(self.grid, key=lambda x: x[0])[0]
+        else:
+            return 0
+
+
 # @aoc_timer
 def part1(puzzle_input):
-    """Solve part 1. Return the required output value."""
+    """Solve part 1. Return the required output value.
 
-    return 1
+    How many units tall will the tower of rocks be after 2022 rocks have stopped falling?
+    """
+    rocks = 0
+    total_jet_movements = len(puzzle_input)
+    jet = 0
+    room = Room()
+
+    while rocks < 2022:
+        logging.debug(f"New rock: {rocks}")
+        # put next shape in at the height of the tower
+        s = Shape(rocks, room.height())
+        while True:
+            move_successful = room.move(s, puzzle_input[jet % total_jet_movements])
+            jet += 1
+
+            if not move_successful:
+                logging.debug(f"Rock stopped {rocks}, height: {room.height()}")
+                rocks += 1
+                break
+
+    return -room.height()
 
 
 # @aoc_timer
--- 2022/tests/test_aoc2022_17.py
+++ 2022/tests/test_aoc2022_17.py
@@ -16,7 +16,7 @@ class Test_AOC2022_17:
 
     def test_1_1(self):
         puzzle_input = load_input("testinput/17_1_1.txt")
-        assert part1(puzzle_input) == 1
+        assert part1(puzzle_input) == 3068
 
     # def test_2_1(self):
     #     puzzle_input = load_input('testinput/17_1_1.txt')
