74588e6791a1450dd17c93aaa9ee7c8fcce5b26a
Bjoern Winkler <bjoern@bjoern-winkler.de>
2023-01-15 17:48:29 +0100

    2022 day 19 wip part 1

--- 2022/solutions/aoc2022_19.py
+++ 2022/solutions/aoc2022_19.py
@@ -2,8 +2,10 @@
 
 import re
 
-# from collections import defaultdict
+from collections import deque, defaultdict
+
 # from utils.aoctools import aoc_timer
+from dataclasses import dataclass
 
 
 def load_input(f_name):
@@ -13,9 +15,6 @@ def load_input(f_name):
     e.g. for loading test inputs, specify `testinput/01_1_1.txt`.
     """
     # return input as list of text lines
-    with open(f_name, "r") as f:
-        puzzle_input.append(line.strip())
-
     regex = re.compile(r"(\d+)")
 
     with open(f_name, "r") as f:
@@ -28,6 +27,47 @@ def load_input(f_name):
     return puzzle_input
 
 
+@dataclass
+class State:
+    minute: int = 1
+    a_ore: int = 0
+    a_clay: int = 0
+    a_obs: int = 0
+    t_ore: int = 0
+    t_clay: int = 0
+    t_obs: int = 0
+    geodes: int = 0
+    r_ore: int = 0
+    r_clay: int = 0
+    r_obs: int = 0
+    r_geode: int = 0
+
+    def __hash__(self) -> int:
+        return (
+            self.minute,
+            self.a_ore,
+            self.a_clay,
+            self.a_obs,
+            self.geodes,
+            self.r_ore,
+            self.r_clay,
+            self.r_obs,
+            self.r_geode,
+        )
+
+
+class Blueprint:
+
+    def __init__(self, recipe) -> None:
+        self.id = recipe[0]
+        # (ore, clay, obsidian)
+        self.robots = {
+            'ore': (recipe[1], 0, 0),
+            'clay': (recipe[2], 0, 0),
+            'obs': (recipe[3], recipe[4], 0),
+            'geode': (recipe[5], 0, recipe[6])
+        }
+
 # @aoc_timer
 def part1(puzzle_input):
     """Solve part 1. Return the required output value.
@@ -39,16 +79,39 @@ def part1(puzzle_input):
     State of a BFS is
     - minute
     - number of robots of each type
-    - number of ore and clay
+    - number of ore and clay and obsidian collected
     - number of geodes opened
 
     Possible optimization:
     - consider same state if number of robots is same for the minute
-      and discard if number of ore, clay and geodes is larger
-    - or, total number of ore and clay (incl spent on robots) is larger
-      for the same minute (regardless of how spent it)
+      and discard if number of ore, clay, obsidian and geodes is larger
+    - or, total number of ore, clay and obsidian (incl spent on robots)
+      and geodes opened is larger for the same minute (regardless of number
+      of robots i.e. how it was spent)
     """
-    print(puzzle_input)
+    result = 0
+    for blueprint in puzzle_input:
+        bp = Blueprint(*blueprint)
+        max_geodes = 0
+        
+        q = deque([State()])
+        seen = set()
+
+        while q:
+            curr = q.popleft()
+            if curr in seen:
+                continue
+
+            # process one round
+            # start construction of any possible robots
+            # these are the valid neighbours
+            for 
+
+            # collect any resources
+
+            # add new robots
+
+            # increase minute and add to queue
 
     return 1
 
