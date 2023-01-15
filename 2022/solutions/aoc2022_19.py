444c001e9709e68a8b4d1d613620a7f76408340e
Bjoern Winkler <bjoern@bjoern-winkler.de>
2023-01-15 17:19:04 +0100

    2022 day 19 start

--- 2022/solutions/aoc2022_19.py
+++ 2022/solutions/aoc2022_19.py
@@ -1,6 +1,7 @@
 # Load any required modules. Most commonly used:
 
-# import re
+import re
+
 # from collections import defaultdict
 # from utils.aoctools import aoc_timer
 
@@ -13,31 +14,41 @@ def load_input(f_name):
     """
     # return input as list of text lines
     with open(f_name, "r") as f:
+        puzzle_input.append(line.strip())
+
+    regex = re.compile(r"(\d+)")
+
+    with open(f_name, "r") as f:
         puzzle_input = []
         for line in f.readlines():
-            puzzle_input.append(line.strip())
-
-    # Extract ints from the input
-    #
-    # signed ints
-    # regex = re.compile(r"(-?\d+)")
-    #
-    # unsigned ints
-    # regex = re.compile(r"(\d+)")
-    #
-    # with open(f_name, "r") as f:
-    #     puzzle_input = []
-    #     for line in f.readlines():
-    #         matches = regex.findall(line.strip())
-    #         if matches:
-    #             puzzle_input.append(list(map(int, matches)))
+            matches = regex.findall(line.strip())
+            if matches:
+                puzzle_input.append(list(map(int, matches)))
 
     return puzzle_input
 
 
 # @aoc_timer
 def part1(puzzle_input):
-    """Solve part 1. Return the required output value."""
+    """Solve part 1. Return the required output value.
+
+    Multiply blueprint ID with largest number of geodes that can be
+    opened in 24 minutes. Add up all these products.
+
+    Run a BFS for each blueprint.
+    State of a BFS is
+    - minute
+    - number of robots of each type
+    - number of ore and clay
+    - number of geodes opened
+
+    Possible optimization:
+    - consider same state if number of robots is same for the minute
+      and discard if number of ore, clay and geodes is larger
+    - or, total number of ore and clay (incl spent on robots) is larger
+      for the same minute (regardless of how spent it)
+    """
+    print(puzzle_input)
 
     return 1
 
@@ -61,5 +72,5 @@ if __name__ == "__main__":
     p2 = part2(puzzle_input)
     print(f"Part 2: {p2}")
 
-# Part 1: Start:  End:
+# Part 1: Start: 17:00 End:
 # Part 2: Start:  End:
--- 2022/tests/test_aoc2022_19.py
+++ 2022/tests/test_aoc2022_19.py
@@ -16,7 +16,7 @@ class Test_AOC2022_19:
 
     def test_1_1(self):
         puzzle_input = load_input("testinput/19_1_1.txt")
-        assert part1(puzzle_input) == 1
+        assert part1(puzzle_input) == 33
 
     # def test_2_1(self):
     #     puzzle_input = load_input('testinput/19_1_1.txt')
