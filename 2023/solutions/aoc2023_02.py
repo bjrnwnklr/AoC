d5762dd4e438f6706e4b8852dc0da979c01913ee
Bjoern Winkler <bjoern@bjoern-winkler.de>
2023-12-02 11:48:23 +0100

    2023 day 2 complete

--- 2023/solutions/aoc2023_02.py
+++ 2023/solutions/aoc2023_02.py
@@ -1,8 +1,9 @@
 # Load any required modules. Most commonly used:
 
-# import re
+import re
+
 # from collections import defaultdict
-# from utils.aoctools import aoc_timer
+from utils.aoctools import aoc_timer
 
 
 def load_input(f_name):
@@ -35,18 +36,61 @@ def load_input(f_name):
     return puzzle_input
 
 
-# @aoc_timer
+@aoc_timer
 def part1(puzzle_input):
     """Solve part 1. Return the required output value."""
+    # rules / constraints
+    config = {"red": 12, "green": 13, "blue": 14}
+    # parse input into games and sets of cubes
+    result = 0
+    for line in puzzle_input:
+        # possible value
+        possible = True
+        # find game id
+        match = re.search(r"(\d+)", line)
+        if match:
+            game = int(match[0])
+        # find other digits with cubes
+        matches = re.findall(r"((\d+) (red|green|blue))", line)
+        if matches:
+            for m in matches:
+                _, n, c = m
+                n = int(n)
+                # check if this is a possible value
+                if n > config[c]:
+                    possible = False
 
-    return 1
+        if possible:
+            result += game
+
+    return result
 
 
-# @aoc_timer
+@aoc_timer
 def part2(puzzle_input):
     """Solve part 2. Return the required output value."""
+    # parse input into games and sets of cubes
+    result = 0
+    for line in puzzle_input:
+        power = 1
+        max_colors = {"red": 0, "green": 0, "blue": 0}
+        # find game id
+        match = re.search(r"(\d+)", line)
+        if match:
+            game = int(match[0])
+        # find other digits with cubes
+        matches = re.findall(r"((\d+) (red|green|blue))", line)
+        if matches:
+            for m in matches:
+                _, n, c = m
+                n = int(n)
+                # check for maximum value of each color
+                max_colors[c] = max(max_colors[c], n)
+        for n in max_colors.values():
+            power *= n
+        result += power
 
-    return 1
+    return result
 
 
 if __name__ == "__main__":
@@ -61,5 +105,10 @@ if __name__ == "__main__":
     p2 = part2(puzzle_input)
     print(f"Part 2: {p2}")
 
-# Part 1: Start:  End:
-# Part 2: Start:  End:
+# Part 1: Start: 11:21 End: 11:40
+# Part 2: Start: 11:41 End: 11:47
+
+# Elapsed time to run part1: 0.00087 seconds.
+# Part 1: 2176
+# Elapsed time to run part2: 0.00078 seconds.
+# Part 2: 63700
--- 2023/tests/test_aoc2023_02.py
+++ 2023/tests/test_aoc2023_02.py
@@ -16,8 +16,8 @@ class Test_AOC2023_02:
 
     def test_1_1(self):
         puzzle_input = load_input("testinput/02_1_1.txt")
-        assert part1(puzzle_input) == 1
+        assert part1(puzzle_input) == 8
 
-    # def test_2_1(self):
-    #     puzzle_input = load_input('testinput/02_1_1.txt')
-    #     assert part2(puzzle_input) == 1
+    def test_2_1(self):
+        puzzle_input = load_input("testinput/02_1_1.txt")
+        assert part2(puzzle_input) == 2286
