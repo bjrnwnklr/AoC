ebdd0a8b464fbbb6ba990b1211d05e2b42c62720
Bjoern Winkler <bjoern@bjoern-winkler.de>
2023-12-03 11:12:33 +0100

    2023 day 3 part 1 completed

--- 2023/solutions/aoc2023_03.py
+++ 2023/solutions/aoc2023_03.py
@@ -1,6 +1,7 @@
 # Load any required modules. Most commonly used:
 
-# import re
+import re
+
 # from collections import defaultdict
 # from utils.aoctools import aoc_timer
 
@@ -17,57 +18,34 @@ def load_input(f_name):
         for line in f.readlines():
             puzzle_input.append(line.strip())
 
-    # Extract ints from the input
-    #
-    # signed ints
-    # regex = re.compile(r"(-?\d+)")
-    #
-    # unsigned ints
-    # regex = re.compile(r"(\d+)")
-    #
-    # with open(f_name, "r") as f:
-    #     puzzle_input = []
-    #     for line in f.readlines():
-    #         matches = regex.findall(line.strip())
-    #         if matches:
-    #             puzzle_input.append(list(map(int, matches)))
-
     return puzzle_input
 
 
-def parse_grid(inp):
-    """Parse the map of numbers and symbols into
-    coordinates and return two objects:
+def parse_grid_regex(inp):
+    """Find numbers including their start position, and symbols.
+    return two objects:
     - set of symbol coordinates
     - list of numbers with two coordinates:
         - row
-        - col of the last digit
-    The start of the digit can be calculated by the length
+        - col of the first digit
+    The end of the digit can be calculated by the length
     of the number as string.
     """
-    symbols = set()
-    numbers = []
+    regex_num = re.compile(r"(\d+)")
+    regex_sym = re.compile(r"([\@+\-#/*$%=&])")
+    re_numbers = []
+    re_symbols = set()
     for r, row in enumerate(inp):
-        n = ""
-        c = 0
-        line = list(row)
-        while line:
-            x = line.pop(0)
-            if not x.isdigit():
-                # check if we were parsing a number
-                # and add its value, row and end column
-                # to the list of numbers
-                if n != "":
-                    numbers.append((int(n), r, c - 1))
-                    n = ""
-                # check if we found a symbol
-                if x != ".":
-                    symbols.add((r, c))
-            elif x.isdigit():
-                n += x
-            c += 1
+        matches = regex_num.finditer(row)
+        if matches:
+            for m in matches:
+                re_numbers.append((int(m[0]), r, m.start()))
+        matches = regex_sym.finditer(row)
+        if matches:
+            for m in matches:
+                re_symbols.add((r, m.start()))
 
-    return symbols, numbers
+    return re_symbols, re_numbers
 
 
 def has_symbol_neighbor(n, r, c, symbols):
@@ -75,8 +53,8 @@ def has_symbol_neighbor(n, r, c, symbols):
     has any symbols has neighbors."""
     # calculate length and end column of number
     l = len(str(n))
-    c_start = c - l + 1
-    c_end = c
+    c_start = c
+    c_end = c_start + l - 1
     # calculate neighboring coordinates and check if they are in
     # symbols
     for dr in [-1, 1]:
@@ -97,7 +75,11 @@ def has_symbol_neighbor(n, r, c, symbols):
 # @aoc_timer
 def part1(puzzle_input):
     """Solve part 1. Return the required output value."""
-    symbols, numbers = parse_grid(puzzle_input)
+    # check that we have the correct number of numbers found
+    # detect numbers with regex
+
+    symbols, numbers = parse_grid_regex(puzzle_input)
+
     result = 0
     for n, r, c in numbers:
         # find neighbors of a number
@@ -126,5 +108,6 @@ if __name__ == "__main__":
     p2 = part2(puzzle_input)
     print(f"Part 2: {p2}")
 
-# Part 1: Start: 9:00 End:
+# Part 1: Start: 9:00 End: 11:10 (wrong end coordinate on numbers
+#                                 at the end of the line)
 # Part 2: Start:  End:
