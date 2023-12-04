1f151ee23a2bcd951ab5a2c708379f268aea4386
Bjoern Winkler <bjoern@bjoern-winkler.de>
2023-12-04 19:08:05 +0100

    2023 day 4 done

--- 2023/solutions/aoc2023_04.py
+++ 2023/solutions/aoc2023_04.py
@@ -2,7 +2,7 @@
 
 # import re
 # from collections import defaultdict
-# from utils.aoctools import aoc_timer
+from utils.aoctools import aoc_timer
 
 
 def load_input(f_name):
@@ -35,18 +35,54 @@ def load_input(f_name):
     return puzzle_input
 
 
-# @aoc_timer
+def parse_card(line):
+    """Parse a card into two lists:
+    - winning numbers
+    - numbers you have
+    """
+    _, numbers = line.split(":")
+    # ignore the card for now - might need later
+    winning, held = numbers.split("|")
+    win_num = list(map(int, winning.strip().split()))
+    held_num = list(map(int, held.strip().split()))
+
+    return win_num, held_num
+
+
+@aoc_timer
 def part1(puzzle_input):
     """Solve part 1. Return the required output value."""
+    result = 0
+    for line in puzzle_input:
+        winning, held = parse_card(line)
+        p = 0
+        for n in held:
+            if n in winning:
+                p += 1
+        if p > 0:
+            result += 2 ** (p - 1)
 
-    return 1
+    return result
 
 
-# @aoc_timer
+@aoc_timer
 def part2(puzzle_input):
     """Solve part 2. Return the required output value."""
+    card_count = len(puzzle_input)
+    # dictionary that has how many of each card we have
+    cards = {i: 1 for i in range(1, card_count + 1)}
+    # iterate through cards
+    for i, line in enumerate(puzzle_input, start=1):
+        winning, held = parse_card(line)
+        cards_won = 0
+        for n in held:
+            if n in winning:
+                cards_won += 1
+                cards[i + cards_won] += cards[i]
 
-    return 1
+    result = sum(n for n in cards.values())
+
+    return result
 
 
 if __name__ == "__main__":
@@ -61,5 +97,10 @@ if __name__ == "__main__":
     p2 = part2(puzzle_input)
     print(f"Part 2: {p2}")
 
-# Part 1: Start:  End:
-# Part 2: Start:  End:
+# Part 1: Start: 18:54 End: 19:04
+# Part 2: Start: 19:05 End: 19:19
+
+# Elapsed time to run part1: 0.00127 seconds.
+# Part 1: 27454
+# Elapsed time to run part2: 0.00137 seconds.
+# Part 2: 6857330
--- 2023/tests/test_aoc2023_04.py
+++ 2023/tests/test_aoc2023_04.py
@@ -16,8 +16,8 @@ class Test_AOC2023_04:
 
     def test_1_1(self):
         puzzle_input = load_input("testinput/04_1_1.txt")
-        assert part1(puzzle_input) == 1
+        assert part1(puzzle_input) == 13
 
-    # def test_2_1(self):
-    #     puzzle_input = load_input('testinput/04_1_1.txt')
-    #     assert part2(puzzle_input) == 1
+    def test_2_1(self):
+        puzzle_input = load_input("testinput/04_1_1.txt")
+        assert part2(puzzle_input) == 30
