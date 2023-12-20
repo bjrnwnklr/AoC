d693e50748a6c4f43ea1215e444a3c8fd372f520
Bjoern Winkler <bjoern@bjoern-winkler.de>
2023-12-20 19:35:55 +0100

    downloaded 2023 15-20

--- 2023/dl.sh
+++ 2023/dl.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for i in {21..23}; do
-	../aocsetup.sh 2023 ${i}
-	curl https://adventofcode.com/2023/day/${i}/input --cookie "session=53616c7465645f5f58b5e33ca37677c1533e760352f12a2abffdb824483d7ec902266a88e062b8ee86fb2962354a04d0" > input/${i}.txt
-	sleep 1
-	curl https://adventofcode.com/2023/day/${i} --cookie "session=53616c7465645f5f58b5e33ca37677c1533e760352f12a2abffdb824483d7ec902266a88e062b8ee86fb2962354a04d0" > puzzles/${i}.html
+for i in {15..16}; do
+	# ../aocsetup.sh 2023 ${i}
+	# curl https://adventofcode.com/2023/day/${i}/input --cookie "session=53616c7465645f5f7e641a062ed6c955b3e46755fa72db6b98bd99d711ffb1b941d3503514b832dac982ee8a5e74ceb8c77087acabc460c992876a8c383ad288" > input/${i}.txt
+	# sleep 1
+	curl https://adventofcode.com/2023/day/${i} --cookie "session=53616c7465645f5f7e641a062ed6c955b3e46755fa72db6b98bd99d711ffb1b941d3503514b832dac982ee8a5e74ceb8c77087acabc460c992876a8c383ad288" > puzzles/${i}.html
 	sleep 2
 done
