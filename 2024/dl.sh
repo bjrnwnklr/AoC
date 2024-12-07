273b724da374be4e9d269a0cbe5d5c17c5d8d4fc
Bjoern Winkler <bjoern@bjoern-winkler.de>
2024-12-07 11:31:31 +0100

    Create 2024 day 2 to 7 properly

--- 2024/dl.sh
+++ 2024/dl.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-for i in {23..25}; do
-	# ../aocsetup.sh 2023 ${i}
-	curl https://adventofcode.com/2023/day/${i}/input --cookie "session=53616c7465645f5f7e641a062ed6c955b3e46755fa72db6b98bd99d711ffb1b941d3503514b832dac982ee8a5e74ceb8c77087acabc460c992876a8c383ad288" > input/${i}.txt
-	# sleep 1
-	# curl https://adventofcode.com/2023/day/${i} --cookie "session=53616c7465645f5f7e641a062ed6c955b3e46755fa72db6b98bd99d711ffb1b941d3503514b832dac982ee8a5e74ceb8c77087acabc460c992876a8c383ad288" > puzzles/${i}.html
-	sleep 2
+for i in {2..7}; do
+	../aocsetup.sh 2024 ${i}
+    # curl https://adventofcode.com/2024/day/${i}/input -A "bjoern@bjoern-winkler.de via curl" --cookie "session=53616c7465645f5f65481baee8754e8c68ce3ca4c9b91ecea745a85fff72fde9bdb3417c3ddc49bc24bb05231d5e4014cfd90ce19dc4fd14e2ffcc572752783a" > input/0${i}.txt
+	# sleep 2
 done
