55491bb8e4653e250806c7a0e3e895fcb97c018f
Bjoern Winkler <bjoern@bjoern-winkler.de>
2024-12-08 13:54:18 +0100

    2024 day 4 part 2 done

--- 2024/solutions/aoc2024_04.py
+++ 2024/solutions/aoc2024_04.py
@@ -3,7 +3,7 @@
 # import re
 from collections import defaultdict
 
-# from utils.aoctools import aoc_timer
+from utils.aoctools import aoc_timer
 
 
 def load_input(f_name):
@@ -33,17 +33,21 @@ n_coords = [
     [(-1, -1), (-2, -2), (-3, -3)],  # up left
 ]
 
+x_coords = [
+    [(-1, -1), (0, 0), (1, 1)],  # top left to bottom right
+    [(1, -1), (0, 0), (-1, 1)],  # bottom left to top right
+]
 
-def get_neighbors(r, c, r_max, c_max):
+
+def get_neighbors(r, c, r_max, c_max, coords):
     """Get the neighboring 3 coordinates for a coordinate in
     row, column pair format.
 
     Neighbors are 3 upwards, 3 downwards, 3 left, 3 right,
     3 diagonal in each direction."""
-    # get horizontal and vertical neighbors. stop if we are leaving
-    # the boundaries
+    # stop if we are leaving the boundaries
     neighbors = []
-    for dx in n_coords:
+    for dx in coords:
         neighbor = []
         in_bounds = True
         for dr, dc in dx:
@@ -63,13 +67,13 @@ def get_neighbors(r, c, r_max, c_max):
 
 
 def get_word(grid, coords):
-    """Return the 4 letters formed by the 4 coordinates
+    """Return the letters formed by the coordinates
     given."""
     word = "".join(grid[(r, c)] for r, c in coords)
     return word
 
 
-# @aoc_timer
+@aoc_timer
 def part1(puzzle_input):
     """Solve part 1. Return the required output value."""
     # get boundaries of the grid
@@ -84,12 +88,10 @@ def part1(puzzle_input):
         for c in range(c_max + 1):
             # check if the letter is an X
             if puzzle_input[(r, c)] == WORD[0]:
-                neighbors = get_neighbors(r, c, r_max, c_max)
+                neighbors = get_neighbors(r, c, r_max, c_max, n_coords)
                 for neighbor in neighbors:
-                    # print(f"{neighbor=}")
                     word_coords = [(r, c)]
                     word_coords.extend(neighbor)
-                    # print(f"Checking word coords: {word_coords}")
                     word = get_word(puzzle_input, word_coords)
                     if word == WORD:
                         # we found a match
@@ -98,11 +100,32 @@ def part1(puzzle_input):
     return result
 
 
-# @aoc_timer
+@aoc_timer
 def part2(puzzle_input):
     """Solve part 2. Return the required output value."""
+    # get boundaries of the grid
+    r_max = max(r[0] for r in puzzle_input.keys())
+    c_max = max(r[1] for r in puzzle_input.keys())
 
-    return 1
+    # iterate through each coordinate, get neighbors
+    # and check if the neighbors spell MAS
+    WORD = "MAS"
+    result = 0
+    for r in range(r_max + 1):
+        for c in range(c_max + 1):
+            # check if the letter is an A
+            if puzzle_input[(r, c)] == WORD[1]:
+                neighbors = get_neighbors(r, c, r_max, c_max, x_coords)
+                matches = 0
+                for neighbor in neighbors:
+                    word_coords = neighbor[:]
+                    word = get_word(puzzle_input, word_coords)
+                    if word == WORD or word == WORD[::-1]:
+                        # we found a match
+                        matches += 1
+                if matches == 2:
+                    result += 1
+    return result
 
 
 if __name__ == "__main__":
@@ -118,4 +141,9 @@ if __name__ == "__main__":
     print(f"Part 2: {p2}")
 
 # Part 1: Start: 11:58 End: 12:41
-# Part 2: Start:  End:
+# Part 2: Start: 13:40 End: 13:53
+
+# Elapsed time to run part1: 0.04089 seconds.
+# Part 1: 2571
+# Elapsed time to run part2: 0.01764 seconds.
+# Part 2: 1992
--- 2024/tests/test_aoc2024_04.py
+++ 2024/tests/test_aoc2024_04.py
@@ -18,6 +18,6 @@ class Test_AOC2024_4:
         puzzle_input = load_input("testinput/04_1_1.txt")
         assert part1(puzzle_input) == 18
 
-    # def test_2_1(self):
-    #     puzzle_input = load_input('testinput/04_1_1.txt')
-    #     assert part2(puzzle_input) == 1
+    def test_2_1(self):
+        puzzle_input = load_input("testinput/04_1_1.txt")
+        assert part2(puzzle_input) == 9
